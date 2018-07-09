70c4517c5297ded96bde85a9f95e8bd04204782a
Pablo <boctulus@gmail.com>
2018-07-09 13:14:01 -0300

    .

--- config/_en_server_/config.php
+++ config/_en_server_/config.php
@@ -9,7 +9,8 @@ return [
 	
 	// JWT
 	'jwt_secret_key' =>'BHH#**@())0))@Jhr&@&#()_hrrK@911kk19))K)_!.S>!_)#I@#(',
-	'token_expiration_time' => 24, // hours
+	'token_expiration_time' => 10, // minutes
+	'extended_token_expiration_time' => 5, // minutes
 	'encryption' => 'HS256'
 	
 	];
