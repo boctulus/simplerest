17e333178828c638447db4fac98234e5ff13d054
Pablo <boctulus@gmail.com>
2019-01-01 15:06:45 -0300

    .

--- config/config.php
+++ config/config.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-	// DB
-	'host' => 'localhost',
-	'db_name' => 'api_sb', // id4434923_api_sb
-	'user' => 'root', // id4434923_root
-	'pass' => '', // gogogo
-	
-	// JWT
-	'jwt_secret_key' =>'BHH#**@())0))@Jhr&@&#()_hrrK@911kk19))K)_!.S>!_)#I@#(',
-	'token_expiration_time' => 5, // minutes
-	'extended_token_expiration_time' => 3, // minutes
-	'encryption' => 'HS256'
-	
+		// DB
+		'host' => 'localhost',
+		'db_name' => 'api_sb', // id4434923_api_sb
+		'user' => 'root', // id4434923_root
+		'pass' => '', // gogogo
+		
+		// JWT
+		'jwt_secret_key' =>'BHH#**@())0))@Jhr&@&#()_hrrK@911kk19))K)_!.S>!_)#I@#(',
+		'token_expiration_time' => 5, // minutes
+		'extended_token_expiration_time' => 3, // minutes
+		'encryption' => 'HS256',
+		'enabled_auth' => false
 	];
