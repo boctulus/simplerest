3a7839c3c69c3ff8fe935c7d29c61304ededa2a5
Pablo <boctulus@gmail.com>
2019-01-01 23:38:07 -0300

    .

--- api/products.php
+++ api/products.php
@@ -1,9 +1,10 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, PATCH, PUT, DELETE');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header('Content-Type: application/json');
+header('access-control-allow-credentials: true');
+header('access-control-allow-headers: content-type'); // : origin, content-type, x-auth-token'
+header('access-control-allow-Methods: GET,HEAD,PUT,PATCH,POST,DELETE'); 
+header('access-control-allow-Origin: *');
+header('content-type: application/json; charset=UTF-8');
 
 /*
 	API Rest examples:
@@ -23,90 +24,90 @@ require_once '../models/product.php';
 $input = file_get_contents("php://input");	
 $data  = json_decode($input);	
 
-logger($input);
+try {
+	$conn = Database::getConnection($config);
+	$product = new Product($conn);
 
-$conn = Database::getConnection($config);
-$product = new Product($conn);
 
+	switch($_SERVER['REQUEST_METHOD'])
+	{
+		/* CREATE */
+		case 'POST':
+			$product->name = $data->name;
+			$product->description = $data->description;
+			$product->size = $data->size;
+			$product->cost = $data->cost;
 
-switch($_SERVER['REQUEST_METHOD'])
-{
-	/* CREATE */
-	case 'POST':
-		logger('create');
-	
-		$product->name = $data->name;
-		$product->description = $data->description;
-		$product->size = $data->size;
-		$product->cost = $data->cost;
-
-		$product->create();
-		if ($product->id){
-			sendData(['id' => $product->id], 201);
-		}	
-		else
-			sendError("Error: Create fails!");
-	break;
-	
-	/* UPDATE */
-	case 'PUT':
-		$id   = $_GET['id'] ?? NULL;
-		
-		if($id == null)
-		{
-			sendError("Lacks id in request",400);
-		}
-		$product->id = $id;
-		$product->name = $data->name;
-		$product->description = $data->description;
-		$product->size = $data->size;
-		$product->cost = $data->cost;
-		
-		if ($product->update()){
-			
-			sendData("OK");
-		}	
-		else
-			sendError("Error: update for id=$id fails!");
-		
-	break;
-	
-	/* DELETE */
-	case 'DELETE':
-		$id   = $_GET['id'] ?? NULL;
-		
-		if($id == null)
-		{
-			sendError("Lacks id in request",400);
-		}
-		$product->id = $id;
-		
-		if($product->delete()){
-			sendData("OK");
-		}	
-		else
-			sendError("Error: delete for id=$id fails!",410);
-			
-	break;
-	
-	/* READ */
-	case 'GET':
-		$id   = $_GET['id'] ?? NULL;
-	
-		if (!$id){
-			$rows = $product->readAll();
-			sendData($rows,200); 
-		}else{ 
-			$product->id = $_GET['id'];
-			if ($product->read() === false)
-				sendError("Not found for id={$_GET['id']}",404);
+			$product->create();
+			if ($product->id){
+				sendData(['id' => $product->id], 201);
+			}	
 			else
-				sendData($product, 200);
-		}
-	break;
-	
-	default:
-		sendError("Invalid http verb",400);
+				sendError("Error: Create fails!");
+		break;
+		
+		/* UPDATE */
+		case 'PUT':
+			$id   = $_GET['id'] ?? NULL;
+			
+			if($id == null)
+			{
+				sendError("Lacks id in request",400);
+			}
+			$product->id = $id;
+			$product->name = $data->name;
+			$product->description = $data->description;
+			$product->size = $data->size;
+			$product->cost = $data->cost;
+			
+			if ($product->update()){
+				
+				sendData("OK");
+			}	
+			else
+				sendData("WARNING: register with id=$id was not updated. Maybe has not changed (?)");
+			
+		break;
+		
+		/* DELETE */
+		case 'DELETE':
+			$id   = $_GET['id'] ?? NULL;
+			
+			if($id == null)
+			{
+				sendError("Lacks id in request",400);
+			}
+			$product->id = $id;
+			
+			if($product->delete()){
+				sendData("OK");
+			}	
+			else
+				sendError("Error: delete for id=$id fails!",410);
+				
+		break;
+		
+		/* READ */
+		case 'GET':
+			$id   = $_GET['id'] ?? NULL;
+		
+			if (!$id){
+				$rows = $product->readAll();
+				sendData($rows,200); 
+			}else{ 
+				$product->id = $_GET['id'];
+				if ($product->read() === false)
+					sendError("Not found for id={$_GET['id']}",404);
+				else
+					sendData($product, 200);
+			}
+		break;
+		
+		default:
+			// other verbs
+			break;
+	}
+} catch (Exception $error) {
+	sendError($error);
 }
-
 	
