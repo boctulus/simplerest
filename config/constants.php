a232328170751655a7cf5273bc80f2f927e1d772
Pablo <boctulus@gmail.com>
2019-01-04 21:49:47 -0300

    more constants

--- config/constants.php
+++ config/constants.php
@@ -9,6 +9,15 @@ if (!defined('CORE_PATH'))
 if (!defined('MODELS_PATH'))
 define('MODELS_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'models/');    
 
+if (!defined('VIEWS_PATH'))
+define('VIEWS_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'views/');  
+
+if (!defined('CONTROLLERS_PATH'))
+define('CONTROLLERS_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'controllers/');  
+
+if (!defined('ASSETS_PATH'))
+define('ASSETS_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'assets/'); 
+
 // helpers at root level
 if (!defined('HELPERS_PATH'))
 define('HELPERS_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'helpers/');   
@@ -16,5 +25,9 @@ define('HELPERS_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'helpers/');
 if (!defined('API_PATH'))
 define('API_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'api/'); 
 
+if (!defined('LIBS_PATH'))
+define('LIBS_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'helpers/');   
+
 if (!defined('VENDOR_PATH'))
 define('VENDOR_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor/'); 
+
