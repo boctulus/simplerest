5812623ec4e608df24fd19477f63624ee869494a
boctulus <boctulus@gmail.com>
2019-09-19 14:58:08 -0300

    fix

--- helpers/debug.php
+++ helpers/debug.php
@@ -58,29 +58,6 @@ function json_var_dump_v2($var){
    return json_encode(ob_get_clean());            
 }     
 
-/*
-	@author mario
-	http://stackoverflow.com/questions/24316347/how-to-format-var-export-to-php5-4-array-syntax
-*/
-function var_export2($var, $indent="") {
-    switch (gettype($var)) {
-        case "string":
-            return '"' . addcslashes($var, "\\\$\"\r\n\t\v\f") . '"';
-        case "array":
-            $indexed = array_keys($var) === range(0, count($var) - 1);
-            $r = [];
-            foreach ($var as $key => $value) {
-                $r[] = "$indent    "
-                     . ($indexed ? "" : var_export2($key) . " => ")
-                     . var_export54($value, "$indent    ");
-            }
-            return "[\n" . implode(",\n", $r) . "\n" . $indent . "]";
-        case "boolean":
-            return $var ? "TRUE" : "FALSE";
-        default:
-            return var_export($var, TRUE);
-    }
-}
 
 /** 
 *	@author https://stackoverflow.com/users/1709587
