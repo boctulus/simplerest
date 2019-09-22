3558a7e5248e090c72b62a8eeef2be6069e207e0
boctulus <boctulus@gmail.com>
2019-09-22 09:29:56 -0300

    .

--- config/constants.php
+++ config/constants.php
@@ -13,6 +13,9 @@ if (!defined('UPLOADS_PATH'))
 if (!defined('LOGS_PATH'))
     define('LOGS_PATH', ROOT_PATH . 'logs'. DIRECTORY_SEPARATOR); 
 
+if (!defined('VENDOR_PATH'))
+    define('VENDOR_PATH', ROOT_PATH . 'vendor'. DIRECTORY_SEPARATOR); 
+
 
 if (!defined('APP_PATH'))
     define('APP_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR);
@@ -27,14 +30,7 @@ if (!defined('VIEWS_PATH'))
     define('VIEWS_PATH', APP_PATH .  'views' . DIRECTORY_SEPARATOR);  
 
 if (!defined('CONTROLLERS_PATH'))
-    define('CONTROLLERS_PATH', APP_PATH . 'controllers' . DIRECTORY_SEPARATOR);  
-
-if (!defined('ASSETS_PATH'))
-    define('ASSETS_PATH', APP_PATH . 'assets' . DIRECTORY_SEPARATOR); 
-
-// helpers at root level
-if (!defined('HELPERS_PATH'))
-    define('HELPERS_PATH', APP_PATH . 'helpers' . DIRECTORY_SEPARATOR);   
+    define('CONTROLLERS_PATH', APP_PATH . 'controllers' . DIRECTORY_SEPARATOR);    
 
 if (!defined('API_PATH'))
     define('API_PATH', CONTROLLERS_PATH . 'api' . DIRECTORY_SEPARATOR); 
@@ -42,5 +38,18 @@ if (!defined('API_PATH'))
 if (!defined('LIBS_PATH'))
     define('LIBS_PATH', APP_PATH . 'libs' . DIRECTORY_SEPARATOR);   
 
-if (!defined('VENDOR_PATH'))
-    define('VENDOR_PATH', APP_PATH . 'vendor'. DIRECTORY_SEPARATOR); 
+
+if (!defined('PUBLIC_PATH'))
+    define('PUBLIC_PATH', ROOT_PATH . 'public' . DIRECTORY_SEPARATOR); 
+
+if (!defined('ASSETS_PATH'))
+    define('ASSETS_PATH', PUBLIC_PATH . 'assets' . DIRECTORY_SEPARATOR); 
+
+if (!defined('IMAGES_PATH'))
+    define('IMAGES_PATH', PUBLIC_PATH . 'images' . DIRECTORY_SEPARATOR);     
+
+if (!defined('CSS_PATH'))
+    define('CSS_PATH', PUBLIC_PATH . 'images' . DIRECTORY_SEPARATOR);   
+
+if (!defined('JS_PATH'))
+    define('JS_PATH', PUBLIC_PATH . 'js' . DIRECTORY_SEPARATOR);       
