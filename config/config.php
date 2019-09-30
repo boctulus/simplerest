ea4eb5983a26b1268dfdb87dd837afdd3eed07ff
boctulus <boctulus@gmail.com>
2019-09-30 16:14:33 -0300

    .

--- config/config.php
+++ config/config.php
@@ -3,27 +3,32 @@
 require_once 'constants.php';
 
 return [
-		'BASE_URL' => '/',   
-		'DEFAULT_CONTROLLER' => 'ProductsController',
+	'BASE_URL' => '/',   
+	'DEFAULT_CONTROLLER' => 'ProductsController',
 
-		'database' => [
-			'host' => 'localhost',
-			'db_name' => 'api_sb', 
-			'user' => 'boctulus', 
-			'pass' => 'gogogo2k'
-		], 
+	'database' => [
+		'host' => 'localhost',
+		'db_name' => 'api_sb', 
+		'user' => 'boctulus', 
+		'pass' => 'gogogo2k'
+	], 
 
-		'debug_mode' => true,
+	'debug_mode'   => true,
+	
+	'enabled_auth' => true,
+
+	'access_token' => [
+		'secret_key' =>'BHH#**@())0))@Jhr&@&#()_hrrK@911kk19))K)_!.S>!_)#I@#(',
+		'expiration_time' => 60,
+		'encryption' => 'HS256'			
+	],
+
+	'refresh_token' => [
+		'secret_key' => '0abc45de0405060708090a0b0c0d0e0f10112131415161718191a1ff1c1d1e1f'
+	],
+
+	'session' => [
+		'secret_key' => 'fabcefdeefdcabcdea457123efda67857123efda67857123efda00234244fabc'
+	]
 		
-		// JWT
-		'jwt_secret_key' =>'BHH#**@())0))@Jhr&@&#()_hrrK@911kk19))K)_!.S>!_)#I@#(',
-		'token_expiration_time' => 600,
-		'encryption' => 'HS256',
-		'enabled_auth' => true,
-
-		// Refresh token
-		'refresh_secret_key' => '0abc45de0405060708090a0b0c0d0e0f10112131415161718191a1ff1c1d1e1f',
-
-		// session
-		'session_secret_key' => 'fabcefdeefdcabcdea457123efda67857123efda67857123efda00234244fabc'
-	];
+];
