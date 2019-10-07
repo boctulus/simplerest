ef3f10916b5f1a6697a79a94b34be229e19ea1ec
boctulus <boctulus@gmail.com>
2019-10-07 18:47:59 -0300

    fix

--- app/api/Users.php
+++ app/api/Users.php
@@ -6,6 +6,13 @@ use simplerest\controllers\MyApiController;
 
 class Users extends MyApiController
 {     
+    protected $scope = [
+        'guest'   => [], 
+        'basic'   => ['read'],
+        'regular' => ['read', 'write'],
+        'admin'   => ['read', 'write']
+    ];
+
     function __construct()
     {
         parent::__construct();
