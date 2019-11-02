a2d15fe3ddae752c63a556c4951868aa17f9b98f
boctulus <boctulus@gmail.com>
2019-11-02 16:17:35 -0300

    .

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -17,17 +17,28 @@ class LoginController extends MyController
 	
 	function login()
 	{
+		// google
 		$google_ctrl = new GoogleController();
 		$gl_client   = $google_ctrl->getClient();
 		$gl_auth_url = $gl_client->createAuthUrl();
 
-		$fb_ctrl = new FacebookController();
-		$fb      = $fb_ctrl->getClient();
-		$helper  = $fb->getRedirectLoginHelper();	
+		// fb
 
-        $permissions = ['email']; // Optional permissions
-        $loginUrl = $helper->getLoginUrl(CALLBACK, $permissions);
+		if (!session_id()) {
+			session_start();
+		}
+		
+		$fb = new \Facebook\Facebook([
+            'app_id' => $this->config['facebook_auth']['app_id'], 
+            'app_secret' => $this->config['facebook_auth']['app_secret'],
+            'default_graph_version' => 'v3.2',
+        ]);
+		
+		$helper = $fb->getRedirectLoginHelper();
 
+		$permissions = ['email'];
+		$loginUrl = $helper->getLoginUrl($this->config['facebook_auth']['callback'], $permissions);
+		
 		$this->view('login.php', [	'title'=>'Ingreso', 
 									'hidenav'=> true, 
 									'gl_auth_url' => $gl_auth_url,
@@ -62,16 +73,18 @@ class LoginController extends MyController
 	}
 	
 	/*
-		Sin terminar
+		callback
 	*/
 	function fb_login(){
 
 		$fb_ctrl = new FacebookController();
 		$fb      = $fb_ctrl->getClient();	
 		$helper  = $fb->getRedirectLoginHelper();
+
+		//$_SESSION['FBRLH_state'] = $_GET['state'];
 		
 		try {
-			$accessToken = $helper->getAccessToken();
+			$oAccessToken = $helper->getAccessToken();
 		} catch(Facebook\Exceptions\FacebookResponseException $e) {
 			// When Graph returns an error
 			echo 'Graph returned an error: ' . $e->getMessage();
@@ -81,52 +94,25 @@ class LoginController extends MyController
 			echo 'Facebook SDK returned an error: ' . $e->getMessage();
 			exit;
 		}
-		
-		if (! isset($accessToken)) {
+			
+		if (isset($oAccessToken)) {
+			$oResponse = $fb->get('/me?fields=id,name,email', $oAccessToken);
+			print_r($oResponse->getGraphUser());
+
+			// session_destroy();
+		} else {
 			if ($helper->getError()) {
-			header('HTTP/1.0 401 Unauthorized');
-			echo "Error: " . $helper->getError() . "\n";
-			echo "Error Code: " . $helper->getErrorCode() . "\n";
-			echo "Error Reason: " . $helper->getErrorReason() . "\n";
-			echo "Error Description: " . $helper->getErrorDescription() . "\n";
+				header('HTTP/1.0 401 Unauthorized');
+				echo "Error: " . $helper->getError() . "\n";
+				echo "Error Code: " . $helper->getErrorCode() . "\n";
+				echo "Error Reason: " . $helper->getErrorReason() . "\n";
+				echo "Error Description: " . $helper->getErrorDescription() . "\n";
 			} else {
-			header('HTTP/1.0 400 Bad Request');
-			echo 'Bad request';
+				header('HTTP/1.0 400 Bad Request');
+				echo 'Bad request';
 			}
 			exit;
 		}
-		
-		// Logged in
-		echo '<h3>Access Token</h3>';
-		var_dump($accessToken->getValue());
-		
-		// The OAuth 2.0 client handler helps us manage access tokens
-		$oAuth2Client = $fb->getOAuth2Client();
-		
-		// Get the access token metadata from /debug_token
-		$tokenMetadata = $oAuth2Client->debugToken($accessToken);
-		echo '<h3>Metadata</h3>';
-		var_dump($tokenMetadata);
-		
-		// Validation (these will throw FacebookSDKException's when they fail)
-		$tokenMetadata->validateAppId('{app-id}'); // Replace {app-id} with your app id
-		// If you know the user ID this access token belongs to, you can validate it here
-		//$tokenMetadata->validateUserId('123');
-		$tokenMetadata->validateExpiration();
-		
-		if (! $accessToken->isLongLived()) {
-			// Exchanges a short-lived access token for a long-lived one
-			try {
-			$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-			} catch (Facebook\Exceptions\FacebookSDKException $e) {
-			echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
-			exit;
-			}
-		
-			echo '<h3>Long-lived</h3>';
-			var_dump($accessToken->getValue());
-		}
-		
 	}
 
 	/*
