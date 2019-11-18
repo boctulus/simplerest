ca3523506c17fbbd1c07a6ae17123358b2ae665e
boctulus <boctulus@gmail.com>
2019-11-18 20:54:06 -0300

    cli support

--- app/core/FrontController.php
+++ app/core/FrontController.php
@@ -10,51 +10,69 @@ class FrontController
 
     static function resolve()
     {
+        global $argv;
+
         $config = include '../config/config.php';
 
-        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $path = preg_replace('/(.*)\/index.php/', '/', $path);
+        if (php_sapi_name() == 'cli'){
+            $_params = $argv;
 
-        if ($config['BASE_URL'] != '/' && strpos($path, $config['BASE_URL']) === 0) {
-            $path = substr($path, strlen($config['BASE_URL']));
-        }   
-
-        if ($path === false || ! Url::url_check($_SERVER['REQUEST_URI']) )
-            Response::getInstance()->sendError('Malformed url', 400); 
-
-        $_params = explode('/', $path);
-
-        if (empty($_params[0]))  
-            array_shift($_params);
-
-        $req = Request::getInstance();    
-
-        // patch
-        if ($_params[0]=='api'){
-            @list($controller) = array_slice($_params,1,1);
-            $params = array_slice($_params,2);
-            $req->setParams($params);     
-            $namespace = 'simplerest\\api\\';
-            
-            $class_name = $namespace . ucfirst($controller); //
-            $method = strtolower($_SERVER['REQUEST_METHOD']);
-        }else{
-            @list($controller, $action) = array_slice($_params,0,2);
-            $params = array_slice($_params,2);
-            $req->setParams($params);
+            @list($controller, $action) = array_slice($_params,1,3);
+            $params = array_slice($_params,3);
             $namespace = 'simplerest\\controllers\\';
 
-            $default_controller_name = substr($config['DEFAULT_CONTROLLER'],0, strlen($config['DEFAULT_CONTROLLER'])-10);
-            $class_file = !empty($controller) ? $controller : $default_controller_name;
+            if (empty($controller))
+                throw new \Exception("Lacks controller specification");
+
+            $class_file = $controller;
             $method = !empty($action) ? $action : self::DEFAULT_ACTION;
     
             $class_name = ucfirst($class_file);
             $class_name = "${namespace}${class_name}Controller";
-        }
-       
-        //if (strpos($class_name,'&') !== false)
-        //    Response::getInstance()->sendError("Malformed url (& instead of ?)", 400); 
+
+        }else{
+            $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+            $path = preg_replace('/(.*)\/index.php/', '/', $path);
+
+            if ($config['BASE_URL'] != '/' && strpos($path, $config['BASE_URL']) === 0) {
+                $path = substr($path, strlen($config['BASE_URL']));
+            }   
+
+            if ($path === false || ! Url::url_check($_SERVER['REQUEST_URI']) )
+                Response::getInstance()->sendError('Malformed url', 400); 
+
+            $_params = explode('/', $path);
+
+            if (empty($_params[0]))  
+                array_shift($_params);
+
+            $req = Request::getInstance();  
             
+        
+            if ($_params[0]=='api'){
+                @list($controller) = array_slice($_params,1,1);
+                $params = array_slice($_params,2);
+                $req->setParams($params);     
+                $namespace = 'simplerest\\api\\';
+                
+                $class_name = $namespace . ucfirst($controller); //
+                $method = strtolower($_SERVER['REQUEST_METHOD']);
+            }else{
+                @list($controller, $action) = array_slice($_params,0,2);
+                $params = array_slice($_params,2);
+                $req->setParams($params);
+                $namespace = 'simplerest\\controllers\\';
+
+                $default_controller_name = substr($config['DEFAULT_CONTROLLER'],0, strlen($config['DEFAULT_CONTROLLER'])-10);
+                $class_file = !empty($controller) ? $controller : $default_controller_name;
+                $method = !empty($action) ? $action : self::DEFAULT_ACTION;
+        
+                $class_name = ucfirst($class_file);
+                $class_name = "${namespace}${class_name}Controller";
+            }
+        }
+          
+       
         if (!class_exists($class_name))
             Response::getInstance()->sendError("Internal error - controller class $class_name not loaded", 500);  
 
