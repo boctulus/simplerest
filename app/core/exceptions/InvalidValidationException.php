89e9e4fbef4bd3c044ff8f4c7bf687d81bbdcdae
Pablo Bozzolo <boctulus@gmail.com>
2019-11-27 14:17:21 -0300

    .

--- app/core/exceptions/InvalidValidationException.php
+++ app/core/exceptions/InvalidValidationException.php
@@ -3,24 +3,5 @@
 namespace simplerest\core\exceptions;
 
 class InvalidValidationException extends \InvalidArgumentException {
-    protected $message = 'Unknown exception';     // Exception message
-    private   $string;                            // Unknown
-    protected $code    = 0;                       // User-defined exception code
-    protected $file;                              // Source filename of exception
-    protected $line;                              // Source line of exception
-    private   $trace;                             // Unknown
 
-    public function __construct($message = null, $code = 0)
-    {
-        if (!$message) {
-            throw new $this('Unknown '. get_class($this));
-        }
-        parent::__construct(_('Data validation error') . ' : '.$message, $code);
-    }
-
-    public function __toString()
-    {
-        return get_class($this) . " '{$this->message}' in {$this->file}({$this->line})\n"
-                                . "{$this->getTraceAsString()}";
-    }
 }
