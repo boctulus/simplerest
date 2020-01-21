cd5602f9eba3eb8cd73f1f654952c0712b1b9f32
Pablo Bozzolo <boctulus@gmail.com>
2020-01-20 22:13:59 -0300

    .

--- app/controllers/MyController.php
+++ app/controllers/MyController.php
@@ -8,15 +8,16 @@ class MyController extends Controller {
     protected $_title;
 
     function __construct(){
-        parent::__construct();
-        $_ctrl = explode('\\',get_class($this));
-        $ctrl  = $_ctrl[count($_ctrl)-1];
-        $this->_title = substr($ctrl,0,strlen($ctrl)-10);
+        parent::__construct();        
     }
 
     function view(string $view_path, array $vars_to_be_passed = null, $layout = 'app_layout.php'){
+        $_ctrl = explode('\\',get_class($this));
+        $ctrl  = $_ctrl[count($_ctrl)-1];
+        $_title = substr($ctrl,0,strlen($ctrl)-10);     
+        
         if(!isset($vars_to_be_passed['title'])){
-            $vars_to_be_passed['title'] = $this->_title;
+            $vars_to_be_passed['title'] = $_title;
         }
 
         parent::view($view_path, $vars_to_be_passed, $layout);
