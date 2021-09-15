9c8757b545775645b34dfb51219c79e1ca27f933
Pablo Bozzolo <boctulus@gmail.com>
2021-09-15 11:33:53 -0500

    Actualizado script de instalación

--- app/controllers/InstallController.php
+++ app/controllers/InstallController.php
@@ -4,6 +4,7 @@ namespace simplerest\controllers;
 
 use simplerest\core\ConsoleController;
 use simplerest\core\Model;
+use simplerest\libs\Schema;
 
 class InstallController extends ConsoleController
 {
@@ -13,38 +14,14 @@ class InstallController extends ConsoleController
         $this->install();
     }
 
-    private function install(){
-        $file = file_get_contents(ETC_PATH . 'db_base.sql');
+    private function install()
+    {
+        Schema::disableForeignKeyConstraints();
         
-        /*
-            Estoy seguro no hay un ; más que para indicar terminación de sentencias
-        */
-        $sentences = explode(';', $file);
-
-        Model::query('SET foreign_key_checks = 0');
-        
-        foreach ($sentences as $sentence){
-            $sentence = trim($sentence);
-
-            if ($sentence == ''){
-                continue;
-            }
-
-            dd($sentence, 'SENTENCE');
-
-            try {
-                $ok = Model::query($sentence);
-            } catch (\Exception $e){
-                dd($e, 'Sql Exception');
-            }
-        }
-
-        /*
-            Luego de crear tablas básicas, corro migraciones para crear el resto
-        */
         $res = shell_exec('php com migrations migrate');
-
-        Model::query('SET foreign_key_checks = 1');
+        dd($res);
+        
+        Schema::enableForeignKeyConstraints();
 
         $res = shell_exec("php com make schema all -f --from:main");
         dd($res);
--- app/helpers/etc.php
+++ app/helpers/etc.php
@@ -2,6 +2,7 @@
 
 use simplerest\libs\Strings;
 use simplerest\libs\DB;
+use simplerest\core\Model;
 
 if (!function_exists('env')){
     function env(string $key, $default_value = null){
@@ -61,13 +62,41 @@ function get_name_id(string $table_name){
     return $class::get()['id_name'];
 }
 
-function generateMigrationFileName($name){
+/*
+    Devuelve un path que puede usarse para crear inyectar una migración "manualmente"
+*/
+function generateMigrationFileName($tb_name){
         
     // 2020_10_28_141833_yyy
     $date = date("Y_m_d");
     $secs = time() - 1603750000;
-    $filename = $date . '_'. $secs . '_' . Strings::camelToSnake($name) . '.php'; 
+    $filename = $date . '_'. $secs . '_' . Strings::camelToSnake($tb_name) . '.php'; 
 
     // destination
     return MIGRATIONS_PATH . $filename;
+}
+
+/*
+    Lee linea por línea y ejecuta sentencias SQL
+*/
+function readSql(string $path){
+    $file = file_get_contents($path);
+    
+    $sentences = explode(';', $file);
+        
+    foreach ($sentences as $sentence){
+        $sentence = trim($sentence);
+
+        if ($sentence == ''){
+            continue;
+        }
+
+        dd($sentence, 'SENTENCE');
+
+        try {
+            $ok = Model::query($sentence);
+        } catch (\Exception $e){
+            dd($e, 'Sql Exception');
+        }
+    }
 }
