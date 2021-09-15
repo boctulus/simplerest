88e55125ddcef921ac152d46af65990e37baa972
Pablo Bozzolo <boctulus@gmail.com>
2021-09-15 16:41:28 -0500

    vistas de login funcionales con nueva tabla user

--- app/core/View.php
+++ app/core/View.php
@@ -11,15 +11,15 @@ class View
 
     function __construct(string $view_path, array $vars_to_be_passed  = null, string $layout = 'app_layout.php', string $footer = null)
     {
-		$this->config = Factory::config();
+		$this->config = config();
 
 		if ($this->config['error_handling']) {
             set_exception_handler([$this, 'exception_handler']);
         }
 
         if (!empty($vars_to_be_passed))
-            extract($vars_to_be_passed);                
-
+            extract($vars_to_be_passed);      
+            
         ob_start();
         include VIEWS_PATH . $view_path;
         $content = ob_get_contents();
