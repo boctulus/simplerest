8ce978701e2fcf0fed9c4c29d4df932069d007d5
Pablo Bozzolo <boctulus@gmail.com>
2021-09-24 11:16:40 -0500

    18n improvement

--- app/helpers/i18n.php
+++ app/helpers/i18n.php
@@ -6,7 +6,10 @@ use simplerest\libs\Strings;
     https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
 */
 function setLang(?string $lang){
-    static $_langs = [];
+    static $langs = [];
+    static $pure_langs = [];
+
+    $selected = null;
 
     if ($lang === NULL || $lang === '*'){
         return;
@@ -15,33 +18,48 @@ function setLang(?string $lang){
     $encode = "UTF-8";
     $throw  = false;
 
-    if (empty($_langs)){
+    if (empty($langs)){
         foreach (new \DirectoryIterator(LOCALE_PATH) as $fileInfo) {
             if($fileInfo->isDot() || !$fileInfo->isDir()) continue;
             
             $filename = $fileInfo->getBasename();
-            $_langs[] = $filename;
+            $langs[]  = $filename;
+            $pure_langs[] = substr($filename, 0, 2);
         } 
     }
 
     /*
         If there is no translation for an specific country then to use any for that language
     */
-
-    $found = false;
     if (!Strings::contains('_', $lang)){
-        foreach ($_langs as $l){
-            if (substr($l, 0, 2) == $lang){
-                $found = true;                
-                $lang  = $l;
-                break;
-            }
-        }      
+        $only_lang = substr($lang, 0, 2);
+        $lang_ix   = array_search($only_lang, $pure_langs);
+        $selected  = $langs[$lang_ix];     
+    } else {
+        // full format
+        $lang_ix   = array_search($lang, $langs);
+
+        if ($lang_ix !== false){
+            $selected  = $langs[$lang_ix];    
+        }
     }
 
-    if (!$found && $throw){
-        throw new \Exception("Invalid lang $lang");
+    /*
+        If every thing fails, last chance
+    */
+    if ($selected === null){
+        $only_lang = substr($lang, 0, 2);
+        $lang_ix   = array_search($only_lang, $pure_langs);
+        $selected  = $langs[$lang_ix];     
     }
 
-    setlocale(LC_ALL, "$lang.$encode");  
+    if ($selected === null){
+        if ($throw){
+            throw new \InvalidArgumentException("Invalid lang $lang");
+        }
+
+        return;        
+    }
+
+    setlocale(LC_ALL, "$selected.$encode");  
 }
