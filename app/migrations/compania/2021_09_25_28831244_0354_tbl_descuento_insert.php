2dba019864f6ee2c47edfff0a3f654cbc7f71531
Pablo Bozzolo <boctulus@gmail.com>
2021-09-25 11:26:43 -0500

    more migration options

--- app/migrations/compania/2021_09_25_28831244_0354_tbl_descuento_insert.php
+++ app/migrations/compania/2021_09_25_28831244_0354_tbl_descuento_insert.php
@@ -16,9 +16,9 @@ class TblDescuento implements IMigration
     public function up()
     {
         Model::query("INSERT INTO tbl_descuento(des_intId, des_varDescuento, des_decDescuento, des_dtimFechaCreacion, des_timFechaActualizacion, est_intIdEstado, usu_intIdCreador, usu_intIdActualizador) VALUES
-(1, 'Descuento uno', 10.00, '2021-09-06 10:54:36', '0000-00-00 00:00:00', 1, 1, 1),
-(2, 'Descuento dos', 20.00, '2021-09-06 10:54:36', '0000-00-00 00:00:00', 1, 1, 1),
-(3, 'Descuento Tres', 30.00, '2021-09-06 10:54:36', '0000-00-00 00:00:00', 1, 1, 1);
+(1, 'Descuento uno', 10.00, '2021-09-06 10:54:36', NULL, 1, 1, 1),
+(2, 'Descuento dos', 20.00, '2021-09-06 10:54:36', NULL, 1, 1, 1),
+(3, 'Descuento Tres', 30.00, '2021-09-06 10:54:36', NULL, 1, 1, 1);
 ");
     }
 }
