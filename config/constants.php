7b5316396801e23a5d85fac17f65f1321d19cfc3
Pablo Bozzolo <boctulus@gmail.com>
2021-10-02 22:38:55 -0500

    .

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,7 @@
 <?php
 
+include __DIR__ . '/msg_const.php';
+
 if (!defined('ROOT_PATH'))
     define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR );
 
---
+++ config/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+    Pre-compiled messages and error codes
+*/
+
+$_messages = <<<MSG
+0001	INVALID_CLASS			"Invalid class"
+1000	VALIDATION_ERROR		"Validation error"
+1001	NOT_BETWEEN				"%s should be less than %s or gretter than %s"
+1002	INVALID_DATA_TYPE 		"It's not a valid %s"
+MSG;
+
+
