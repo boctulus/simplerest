4046e809cb78daa1beb8c42cf00ebb1a29bb9465
Pablo Bozzolo <boctulus@gmail.com>
2021-10-03 22:25:46 -0500

    .

--- app/views/error.php
+++ app/views/error.php
@@ -1,13 +1,13 @@
-<h1 style="font-size:450%;"><?= $status ?></h1>
+<h1 style="font-size:650%; font-weight: bold; color: red;"><?= $status ?></h1>
 
 <br/>
 <img src="<?= assets('img/warning.svg.png') ?>" style="width:25%" />
 
 
 <div style="margin-top: 30px; font-size:150%;">
-Error type: <?= $type ?><br/>
-Code: <?= $code ?><br/>
-Message: <?= $message ?? '' ?><br/>
-Detail: <?= $detail ?? '' ?>
+    Error type: <?= $type ?><br/>
+    Code: <?= $code ?><br/>
+    Message: <?= $message ?? '' ?><br/>
+    Detail: <?= $detail ?? '' ?>
 </div>
 
