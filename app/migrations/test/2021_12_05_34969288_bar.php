7413abab8b40f5e33f32fc0c560f7bcfb7dbb3ce
Pablo Bozzolo <boctulus@gmail.com>
2021-12-05 11:22:44 -0500

    fix

---
+++ app/migrations/test/2021_12_05_34969288_bar.php
@@ -0,0 +1,22 @@
+<?php
+
+use simplerest\core\interfaces\IMigration;
+use simplerest\libs\Factory;
+use simplerest\libs\Schema;
+use simplerest\core\Model;
+use simplerest\libs\DB;
+
+class Bar implements IMigration
+{
+    /**
+	* Run migration.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        $sc->renameTableTo(eee)
+
+    }
+}
+
