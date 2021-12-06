885ef33d1e0fc6041d2e54ceb8e5383367b30f5e
Pablo Bozzolo <boctulus@gmail.com>
2021-12-06 08:13:06 -0500

    improved + doc

--- app/models/TblEmpresaModel.php
+++ app/models/TblEmpresaModel.php
@@ -4,7 +4,7 @@ namespace simplerest\models;
 
 use simplerest\core\Model;
 use simplerest\libs\ValidationRules;
-use simplerest\models\MyModel;
+use simplerest\models\MyModel;	
 use simplerest\models\schemas\legion\TblEmpresaSchema;
 
 class TblEmpresaModel extends MyModel
