5c3d632097cbfda95348eee7ec7c472b56e6b6bc
Pablo Bozzolo <boctulus@gmail.com>
2021-12-07 21:17:08 -0500

    .

--- app/controllers/api/Me.php
+++ app/controllers/api/Me.php
@@ -4,16 +4,12 @@ namespace simplerest\controllers\api;
 
 global $api_version;
 
-if (!file_exists(CORE_PATH . 'api'. DIRECTORY_SEPARATOR . $api_version . DIRECTORY_SEPARATOR . 'ApiController.php')){  
-    \simplerest\core\Response::getInstance()->sendError("Unsupported API version for $api_version", 400);
+/*
+    Agregue según se vayan creando versiones
+*/
+
+if ($api_version == 'v1'){
+    class Me extends \simplerest\core\api\v1\MySelf { }
+    return;
 }
 
-class_alias("\simplerest\core\api\\$api_version\MySelf", 'simplerest\controllers\api\Myself');
-
-class Me extends MySelf
-{ 
-    function __construct(){
-        parent::__construct();  
-    }
-     
-}  
