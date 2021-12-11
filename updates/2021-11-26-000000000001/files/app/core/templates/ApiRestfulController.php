9e46724b784b0dbd66c62ce726b885adf6cbb688
Pablo Bozzolo <boctulus@gmail.com>
2021-12-11 15:28:57 -0500

    mini-merge con Schema pending changes

---
+++ updates/2021-11-26-000000000001/files/app/core/templates/ApiRestfulController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace simplerest\controllers\api;
+
+use simplerest\controllers\MyApiController; 
+
+class __NAME__ extends MyApiController
+{ 
+    static protected $soft_delete = __SOFT_DELETE__;
+    static protected $connect_to = [
+		
+	];
+
+    function __construct()
+    {       
+        parent::__construct();
+    }        
+} 
