8901fddbb152eeb98c594237decd9efdc486adef
Pablo Bozzolo <boctulus@gmail.com>
2021-12-12 18:30:31 -0500

    .

--- updates/2021-12-13-0.0.5-alpha/files/app/core/interfaces/IDbAccess.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace simplerest\core\interfaces;
-
-interface IDbAccess {
-    public function getDbAccess($user_id);
-    public function hasDbAccess($user_id, string $db_connection);
-}
--- updates/2021-12-13-0.0.5-alpha/files/app/core/interfaces/ISubResources.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace simplerest\core\interfaces;
-
-Interface ISubResources {
-    static function getSubResources(string $table, Array $connect_to, ?Object &$instance = null, ?string $tenant_id = null);
-} 
