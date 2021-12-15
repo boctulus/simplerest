f1b76b4940b8f3f3499e956352abcb76a20c7930
Pablo Bozzolo <boctulus@gmail.com>
2021-12-14 22:46:32 -0500

    .

--- updates/2021-12-12-0.5.0-alpha/batches/000-migrations.php
+++ updates/2021-12-12-0.5.0-alpha/batches/000-migrations.php
@@ -4,7 +4,6 @@
     use simplerest\controllers\MigrationsController;
     use simplerest\libs\Strings;
     use simplerest\controllers\UpdateController;
-    use simplerest\controllers\MakeController;
     use simplerest\core\interfaces\IUpdateBatch;
 
     /*
@@ -15,13 +14,10 @@
     {
         function run() : bool{
             $mgr = new MigrationsController();
-            $mk = new MakeController();
 
             $tenant = 'main';
+            //StdOut::hideResponse();
 
-            //php com make schema migrations --from:main
-            $mk->schema("migrations", "--from:main", "--force");
-            
             // Debo correr solo las migraciones que están dentro del update
             $dir = UpdateController::$update_path . 'files/app/migrations/';
             $mgr->migrate("--to=$tenant", "--dir=" . $dir);
