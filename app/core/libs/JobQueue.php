f0a92d456013e57fd340f9b37163a5196da4cdf7
Pablo Bozzolo <boctulus@gmail.com>
2022-01-27 12:01:00 -0500

    .

--- app/core/libs/JobQueue.php
+++ app/core/libs/JobQueue.php
@@ -43,25 +43,8 @@ class JobQueue
         ]);
     }
 
-    /*
-        Funciona con un solo worker
-
-        Habría que ver si con generators se podría hacer algo.
-    */
-    public function exec(){  
-        // La cola debería estar en una base de datos...... y pues encolar es ingresar un registro
-        // que podría ser el objeto serializado o el nombre del job + params   
-        
-        // Así cuando un Worker termina de procesar un Job solo debe revisar en la DB si hay jobs pendientes
-        // ... y los procesa
-        
-        while (!$this->job_queue->isEmpty()){
-            $arr    = $this->job_queue->dequeue();
-            $job    = $arr['object'];
-            $params = $arr['params'];
-
-            $ret = $job->run(...$params);
-        }
+    public function exec(int $workers = 1){  
+        System::runInBackground("php com worker");
     }
 
 }
