3939edad4544a932b66a466362e1896a81acfd83
Pablo Bozzolo <boctulus@gmail.com>
2022-01-29 18:55:04 -0500

    .

--- app/controllers/Hello2Controller.php
+++ app/controllers/Hello2Controller.php
@@ -3,12 +3,38 @@
 namespace simplerest\controllers;
 
 use simplerest\core\controllers\ConsoleController;
+use simplerest\core\libs\Form;
 
 class Hello2Controller extends ConsoleController
 {
-    function __construct()
-    {
-        dd('HERE THERE');
+    function __construct(){
+    }
+
+    function index(){
+        $f = new Form();
+
+        $f->text('nombre', null, [
+            'style' => "background-color:blue"
+        ]);
+
+        $f->select('sexo', 'varon', [
+            'varon' => 1,
+            'mujer' => 2
+        ]);
+
+        $f->range('edad', 0, 99, 10);
+
+        $f->area('comment', 'bla bla');
+        $f->url("Linkedin");
+
+        $f->color("color", "Color");
+
+        $f->reset("limpiar", "limpiar");
+        $f->submit("enviar", "enviar");
+
+        $f->link_to("www.google.com", 'Google');
+
+        return $f->render();
     }
 }
 
