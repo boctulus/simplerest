86a2c11d22d72af0586d2c7c91e1cfabf683c17a
Pablo Bozzolo <boctulus@gmail.com>
2022-01-29 20:49:53 -0500

    .

--- app/views/app_layout.php
+++ app/views/app_layout.php
@@ -8,9 +8,17 @@
 
     <base href="<?= base_url(); ?>">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="<?= assets('css/core.css"') ?>" rel="stylesheet"/>
+    <!-- ico -->
+    <link rel="shortcut icon" href="<?= assets('img/favicon.ico') ?>" />
+
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- ICONOS FONTAWESOME -->
+    <script src="https://kit.fontawesome.com/3f60db90e4.js" crossorigin="anonymous"></script>
+
     
     <!--
         Me traigo los nombres de las keys con que debo armar los JSON
@@ -22,11 +30,29 @@
         let $__password = '<?php echo $__password ?? null; ?>';
     </script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- TEMPLATE STARTUI -->
+    <link rel="stylesheet" href="<?= assets('css/lib/lobipanel/lobipanel.min.css') ?>">
+    <link rel="stylesheet" href="<?= assets('css/separate/vendor_css/lobipanel.min.css') ?>">
+    <link rel="stylesheet" href="<?= assets('css/lib/jqueryui/jquery-ui.min.css') ?>">
+    <link rel="stylesheet" href="<?= assets('css/separate/pages/widgets.min.css') ?>">
+    <link rel="stylesheet" href="<?= assets('css/lib/font-awesome/font-awesome.min.css') ?>">
+    <link rel="stylesheet" href="<?= assets('css/lib/bootstrap/bootstrap.min.css') ?>">
+
+    <link rel="stylesheet" href="<?= assets('css/separate/vendor_css/bootstrap-select/bootstrap-select.min.css') ?>">
+
+    <link rel="stylesheet" href="<?= assets('css/separate/vendor_css/bootstrap-datetimepicker.min.css') ?>">
+
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="<?= assets('css/lib/datatables-net/datatables.min.css') ?>">
+    <link rel="stylesheet" href="<?= assets('css/lib/datatables-net/datatables-net.min.css') ?>">
+
+    <!-- FILEPOND -->
+    <link rel="stylesheet" href="<?= assets('js/plugins/filepond/dist/filepond.css') ?>">
+
     <script src="<?= assets('js/helpers.js') ?>"></script>
     <script src="<?= assets('js/login.js') ?>"></script>
-    <script src="<?= assets('js/jqtable.js') ?>"></script>
+
+    <link  rel="stylesheet" href="<?= assets('css/core.css"') ?>"/>
 
 </head>
 <body>
