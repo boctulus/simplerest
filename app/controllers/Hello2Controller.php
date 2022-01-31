a1985ad1429e20a7504f383a417d716eab06a9c3
Pablo Bozzolo <boctulus@gmail.com>
2022-01-31 10:53:58 -0500

    .

--- app/controllers/Hello2Controller.php
+++ app/controllers/Hello2Controller.php
@@ -3,7 +3,6 @@
 namespace simplerest\controllers;
 
 use simplerest\core\controllers\Controller;
-use simplerest\core\libs\Html;
 use simplerest\core\libs\Form;
 
 class Hello2Controller extends Controller
@@ -12,68 +11,8 @@ class Hello2Controller extends Controller
         parent::__construct();
     }
 
-    function index(){      
-        Form::macro('salutor', function($name, $adj)
-        {
-            return "<span/>Hello $adj $name</span>";
-        });
-
-        
-        $f = new Form();
-
-        $f->h(3, "Datos");
-        
-        $f->tag('hr');
-
-        $f->p("Hola mundo cruel");
-
-        $f->div(function($form){
-            $form->span('@', [
-                'id'    => 'basic-addon',
-                'class' => 'input-group-text'
-            ]);
-            $form->text('nombre', null, [
-                "placeholder" => "Username"
-            ]);
-        }, [
-            "class" => "input-group mb-3"
-        ]);
-
-        $f->select('sexo', 'varon', [
-            'varon' => 1,
-            'mujer' => 2
-        ]);
-
-        $f->label("edad", "Edad");
-        $f->range('edad', 0, 99, 10);
-
-        $f->radio("civil", "casado");
-        $f->radio("civil", "soltero");
-
-        $f->checkbox("hijos", "Hijos", true);
-
-        $f->url("Linkedin");
-
-        $f->label("comment", "Algo que desea agregar:");
-        $f->area('comment', 'bla bla');
-
-        $f->button("comprar", "Comprar");
-
-        $f->reset("limpiar", "limpiar");
-        $f->submit("enviar", "enviar");
-
-        $f->br();
-
-        $f->insert(Form::salutor("Isabel", "bella"));
-        
-        $f->link_to("www.solucionbinaria.com", 'SolucionBinaria .com', [
-            'class' => 'mb-3'
-        ]);
-
-
-        $this->view('hello2.php', [
-            'formx' => $f->render()
-        ]);
+    function index(){
+        $this->view('hello2.php');
     }
 }
 
