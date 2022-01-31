069b5b79c323ea60880c56ee814792f15e1c8998
Pablo Bozzolo <boctulus@gmail.com>
2022-01-30 21:49:22 -0500

    .

--- app/core/libs/Form.php
+++ app/core/libs/Form.php
@@ -2,71 +2,21 @@
 
 namespace simplerest\core\libs;
 
-use PDO;
-use simplerest\core\Model;
-use simplerest\core\libs\DB;
-use simplerest\core\libs\Factory;
-
 /*
     Falta decorar con las clases de Boostrap 5
 */
-class Form
+class Form extends Html
 {
     protected $url;
     protected $method;
-    protected $html;
-    protected $class = '';
-    protected $pretty = true;
-    protected $id_as_name = false;
-    protected $id_eq_name = true;
-
+    
     function __construct(?string $url = null, ?string $method = null) {
         $this->url = $url;
         $this->method = $method;
     }
 
-    function prety(bool $state){
-        $this->pretty = $state;
-    }
-
-    /*
-        Use Id instead of name attribute
-    */
-    function useIdAsName(bool $state){
-        $this->id_as_name = $state;
-
-        if ($this->id_as_name){
-            $this->id_eq_name = false;
-        }
-    }
-
-    /*
-        Copy name attribute into id one
-    */
-    function setIdAsName(bool $state){
-        $this->id_eq_name = $state;
-
-        if ($this->id_eq_name){
-            $this->id_as_name = false;
-        }
-    }
-
     function setUrl(string $url){
         $this->url = $url;
-    }
-
-    protected function add(string $html){
-        $this->html .= ' '. $html;
-        return $this;
-    }
-
-    protected function attributes(Array $attributes) : string{
-        $_att = [];
-        foreach ($attributes as $att => $val){
-            $_att[] = "$att=\"$val\"";
-        }
-
-        return implode(' ', $_att);
     }
 
     /*
@@ -107,36 +57,6 @@ class Form
         }
 
         return call_user_func_array(array($this, $method), $args);
-    }
-
-    protected function tag(string $type, ?string $name = null, ?string $value = '', Array $attributes = [], Array $plain_attr = [])
-    {   
-        if (!is_null($name)){
-            if ($this->id_eq_name){
-                $attributes['id'] = $name;
-                $attributes['name'] = $name;
-            } else if ($this->id_as_name){
-                $attributes['id'] = $name;
-            } else {
-                $attributes['name'] = $name;
-            }   
-        }
-        
-        $attributes['class'] = $attributes['class'] ?? '';
-
-        if (!empty($this->class)){
-            $attributes['class'] .= ' ' . $this->class;
-        } 
-
-        $att_str = $this->attributes($attributes);
-        $p_atr   = implode(' ', $plain_attr);
-
-        // en principio asumo que abre y cierra
-        return "<$type $att_str $p_atr>$value</$type>";
-    }
-
-    function span(string $text, Array $attributes = []){
-        return $this->add($this->tag('span', null, $text, $attributes));
     }
 
     protected function input(string $type, ?string $name = null, ?string $default_value = null, Array $attributes = [], Array $plain_attr = [])
@@ -224,7 +144,7 @@ class Form
         return $this->input('month', $name, $default_value, $attributes); 
     }
 
-    protected function time(string $name, ?string $default_value = null, Array $attributes = []){
+    protected function inputTime(string $name, ?string $default_value = null, Array $attributes = []){
         return $this->input('time', $name, $default_value, $attributes); 
     }
 
@@ -358,67 +278,12 @@ class Form
         return $this->input('hidden', $name, $value, $attributes);
     }
 
-    function link_to(string $url, string $anchor, Array $attributes = []){
-        if (Strings::startsWith('www.', $url)){
-            $url = "http://$url";
+    function render(string $enclosing_tag = null, Array $attributes = [], bool $pretty = true) : string {
+        if (is_null($enclosing_tag)){
+            $enclosing_tag = 'form';
         }
 
-        $att = $this->attributes($attributes);
-        return $this->add("<a href=\"$url\" $att>$anchor</a>");
+        return parent::render($enclosing_tag, $attributes, $pretty);
     }
-
-    /*
-        Form::macro('myField', function()
-        {
-            return '<input type="awesome">';
-        });
-
-        Calling A Custom Form Macro
-
-        echo Form::myField();
-
-    */
-    function macro(string $name, callable $render_fn){
-
-    }
-
-    function group(callable $closure, string $tag = 'div', Array $attributes = []){
-        $f = new Form();
-        call_user_func($closure, $f);
-        
-        return $this->add($f->render($tag, $attributes));
-    }
-
-    function div(callable $closure, $attributes){
-        return $this->group($closure, 'div', $attributes);
-    }
-
-    static function beautifier(string $html){
-        $config = array(
-        'indent'         => true,
-        'output-xhtml'   => true,
-        'wrap'           => 200);
-    
-        // Tidy
-        $tidy = new \tidy;
-        $tidy->parseString($html, $config, 'utf8');
-        $tidy->cleanRepair();
-
-        ob_start();
-        echo $tidy;
-        return ob_get_clean();
-    }
-
-    function render(string $enclosing_tag = 'form', Array $attributes = [], bool $pretty = true){
-        $ret = $this->html;
-
-        if (!empty($enclosing_tag)){
-            $att = $this->attributes($attributes);
-            $ret = "<$enclosing_tag $att>{$ret}</$enclosing_tag>";
-        }
-        
-        return ($pretty && $this->pretty) ? static::beautifier($ret) : $ret;
-    }
-
 }
 
