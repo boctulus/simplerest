2afeee06beab8dab4896d902ae8f0cae96cbeccc
Pablo Bozzolo <boctulus@gmail.com>
2022-02-01 20:44:05 -0500

    .

--- app/libs/Bt5Form.php
+++ app/libs/Bt5Form.php
@@ -6,6 +6,39 @@ use simplerest\core\libs\Form;
 
 class Bt5Form extends Form
 {   
+    protected $classes = [
+        "text"           => "form-control",
+        "number"         => "form-control",
+        "password"       => "form-control",
+        "email"          => "form-control",
+        "file"           => "form-control",
+
+        "date"           => "form-control",
+        "time"           => "form-control",
+        "datetime_local" => "form-control",
+        "month"          => "form-control",
+        "week"           => "form-control",
+        "image"          => "form-control",
+        "range"          => "form-control",
+        "tel"            => "form-control",
+        "url"            => "form-control",
+        "area"           => "form-control",
+
+        "select"         => "form-select",
+
+        "checkbox"       => "form-check-input" ,
+        "radio"          => "form-check-input" ,
+
+        "label"          => "form-check-label",
+
+        "submit"         => "btn btn-primary",
+        "reset"          => "btn btn-primary",
+        "inputButton"    => "btn btn-primary",
+
+        "inputGroup"     => "input-group",
+        "checkGroup"     => "form-check"
+    ];
+
     function __construct() { }
 
     function switch(string $name, string $text, bool $checked = false, Array $attributes = []){
