3ab313358a7fb99e60c7f54a8940585d6ebc068f
Pablo Bozzolo <boctulus@gmail.com>
2022-02-04 14:40:19 -0500

    Now Form has all static methods

--- app/views/hello/hello2.php
+++ app/views/hello/hello2.php
@@ -1,89 +1,87 @@
 <?php
 
-use simplerest\core\libs\Form;
 use simplerest\core\libs\Bt5Form; 
 use simplerest\core\libs\Tag;
 
-Form::macro('salutor', function($name, $adj)
+
+Tag::registerBuilder(\simplerest\core\libs\Bt5Form::class);
+
+Bt5Form::macro('salutor', function($name, $adj)
 {
     return "<span/>Hello $adj $name</span>";
 });
 
+Bt5Form::setIdAsName();
+
 ?>
 
-<div class = "row">
+<div class = "row mt-5">
     <div class = "col-6 offset-3">
-    <?php   
 
-    $f = new Bt5Form();
+    <?php 
+    
+    echo tag('h3')->text("Datos")->class('mb-3');
 
-    $f
-    ->setIdAsName()
+    echo Bt5Form::dataList(listName:'datalistOptions', id:'occupation', options:[
+        'programador',
+        'software engenierer'
+    ], placeholder:'Escriba aquí', label:'Ocupación');
 
-    ->h(3, "Datos")
+    /*
+        El tag 'hr' ni siquiera está definido en la clase Html
+    */
+    echo tag('hr')->style('color:red');
 
-    ->tag(type:'hr', style:'color:grey')
+    echo tag('p')->text("Hola mundo cruel");
 
-    ->p("Hola mundo cruel")
+	//echo tag('color')->name('my_color')->text('Color')->id('c1');
+	
 
-    // ->insert(
-	// 	tag('color')->name('my_color')->text('Color')->id('c1')
-	// )
+    echo Bt5Form::div(content:[
+        tag('span')->text('@')->id('basic-addon')->class('input-group-text'),
+        tag('text')->name('nombre')->placeholder("Username")
+    ],
+        class:"input-group mb-3"
+    );
 
-    ->div(function($form){
-        $form->span('@', [
-            'id'    => 'basic-addon',
-            'class' => 'input-group-text'
-        ]);
-        $form->text('nombre', [
-            "placeholder" => "Username"
-        ]);
-    }, [
-        "class" => "input-group mb-3"
-    ])
-
-    ->select(name:'sexo', options:[
+    echo Bt5Form::select(name:'sexo', options:[
         'varon' => 1,
         'mujer' => 2
-    ], defult:1, placeholder:'Su sexo', attributes:['class' => 'my-3'])
+    ], default:1, placeholder:'Su sexo', attributes:['class' => 'my-3']);
 
-    ->label("edad", "Edad")
-    ->range(name:'edad', min:0, max:99, default:10, class:'my-3')
+    echo tag('label')->name("edad")->text("Edad");
+    echo Bt5Form::range(name:'edad', min:0, max:99, default:10, class:'my-3');
 
-    ->checkGroup(function($h){
-        $h->radio("soltero", true, ['id' => 'soltero']);
-    }, ['class' => 'my-3'])
+    echo tag('checkGroup')->content([
+        Bt5Form::radio(name:'civil', text:"soltero", checked:true, id:'soltero')
+    ])->class('my-3');
 
-    ->checkGroup(function($h){
-        $h->radio("casado", false, ['id' => 'casado']);
-    }, ['class' => 'my-3'])
+    echo tag('checkGroup')->content([
+        Bt5Form::radio(name:'civil', text:"casado", checked:true, id:'casado')
+    ])->class('my-3');
 
+    
+    echo Bt5Form::switch(id:"hijos", text:"Hijos", checked:true);
 
-    ->switch(id:"hijos", text:"Hijos", checked:true)
+    echo Bt5Form::url(default:"Linkedin", class:"mt-3");
 
-    ->url("Linkedin")
+    echo Bt5Form::label(id:"comment", placeholder:"Algo que desea agregar:", class:'mt-3');
+    echo Bt5Form::area(id:'comment', default:'bla bla', class:'my-3');
 
-    ->label(id:"comment", placeholder:"Algo que desea agregar:", attributes:['class' => 'mt-3'])
-    ->area(id:'comment', default:'bla bla', attributes:['class' => 'my-3'])
+    echo Bt5Form::inputButton(id:"comprar", value:"Comprar");
+    echo Bt5Form::reset(id:"limpiar", value:"Limpiar", class:'mx-3');
+    echo Bt5Form::submit(id:"enviar", value:"enviar");
 
-    ->inputButton(id:"comprar", value:"Comprar")
+    echo Bt5Form::br();
 
-    ->reset(id:"limpiar", value:"Limpiar")
-    ->submit(id:"enviar", value:"enviar")
+    echo tag('img')->src(assets('img/personal_data.png'))->id('i1')->class('img-fluid');
 
-    ->br()
+    echo Bt5Form::br();
 
-    ->img(assets('img/personal_data.png'), ['id' => 'i1', 'class' => 'img-fluid'])
+    echo Bt5Form::salutor("Isabel", "bella"); 
+    echo ' ';
+    echo Bt5Form::link_to(href:"www.solucionbinaria.com", anchor:'SolucionBinaria .com', class:'mb-3 text-success');
 
-    ->br()
-
-    ->insert(Form::salutor("Isabel", "bella"))
-
-    ->link_to("www.solucionbinaria.com", 'SolucionBinaria .com', [
-        'class' => 'mb-3 text-success'
-    ]);
-
-    echo $f->render();
     ?>
     </div>
 </div>
