355f9f70caff6e726184c92babfcaa9c49d9b54d
Pablo Bozzolo <boctulus@gmail.com>
2022-02-21 21:41:59 -0500

    .

--- app/views/hello/test.php
+++ app/views/hello/test.php
@@ -13,34 +13,15 @@ Tag::registerBuilder(\simplerest\core\libs\HtmlBuilder\Bt5Form::class);
 <div class="row mt-5">
   <div class="col-4 offset-4">
 
-      <div class="card">
-        <img src="<?= assets('img/warning.svg.png') ?>" width="300px" class="img-fluid" alt="...">
+    <!-- ok -->
+    <button 
+    type="button" 
+    class="btn btn-lg btn-danger popovers" 
+    data-bs-toggle="popover" 
+    title="Popover title" 
+    data-bs-placement = "bottom"
+    data-bs-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
 
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div>
-
-
-        <div class="card" aria-hidden="true">
-          <img src="<?= assets('img/warning.svg.png') ?>" width="300px" class="img-fluid" alt="...">
-          <div class="card-body">
-            <h5 class="card-title placeholder-glow">
-              <span class="placeholder col-6"></span>
-            </h5>
-            <p class="card-text placeholder-glow">
-              <span class="placeholder col-7"></span>
-              <span class="placeholder col-4"></span>
-              <span class="placeholder col-4"></span>
-              <span class="placeholder col-6"></span>
-              <span class="placeholder col-8"></span>
-            </p>
-            <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-3"></a>
-          </div>
-        </div>
-     
 
   </div>
 </div>
@@ -50,4 +31,8 @@ Tag::registerBuilder(\simplerest\core\libs\HtmlBuilder\Bt5Form::class);
     var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
     myModal.show()
   });
+
+  var popover = new bootstrap.Popover(document.querySelector('.popovers'), {
+  container: 'body'
+  })
 </script>
