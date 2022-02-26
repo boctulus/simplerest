721338d3e02a6ee36a6c3b292c4fe307d609bc00
Pablo Bozzolo <boctulus@gmail.com>
2022-02-26 10:57:50 -0500

    sizing

--- app/views/hello/test.php
+++ app/views/hello/test.php
@@ -13,29 +13,57 @@ Tag::registerBuilder(\simplerest\core\libs\HtmlBuilder\Bt5Form::class);
 
 <div class="container mt-5">
 
-    
+  <div class="w-25 p-3" style="background-color: #eee;">Width 25%</div>
+  <div class="w-50 p-3" style="background-color: #eee;">Width 50%</div>
   <?php
+    echo tag('div')->content(
+      'Width 75%'
+    )
+    ->w(75)
+    ->bg('warning')
+    ->class('p-3');
 
-    echo tag('card')->style('width: 18rem;')->class('my-3')
-    // ->content(
-    //   tag('cardImgTop')->src(asset('img/mail.png'))
-    // )
-    ->body([   
-      tag('cardTitle')->text('Some title'),        
-      tag('cardText')->text('Some quick example text to build on the card title and make up the bulk of the cards content.'),
-      tag('inputButton')->value('Go somewhere')
-    ]);
+  ?>
+  <div class="w-100 p-3" style="background-color: #eee;">Width 100%</div>
+  <?php
+    echo tag('div')->content(
+      'Width auto'
+    )
+    ->w('auto')
+    ->bg('warning')
+    ->class('p-3');
 
+  ?>
+    
+  <div style="height: 100px; background-color: rgba(255,0,0,0.1);">
+    <div class="h-25 d-inline-block" style="width: 120px; background-color: rgba(0,0,255,.1)">Height 25%</div>
+    <div class="h-50 d-inline-block" style="width: 120px; background-color: rgba(0,0,255,.1)">Height 50%</div>
 
-      echo tag('card')->style('width: 18rem;')->class('my-3')
-    // ->content(
-    //   tag('cardImgTop')->src(asset('img/mail.png'))
-    // )
-    ->header(tag('cardTitle')->text('Some title'))
-    ->body([           
-      tag('cardText')->text('Some quick example text to build on the card title and make up the bulk of the cards content.'),
-      tag('inputButton')->value('Go somewhere')
-    ]);
+    <?php
+      echo tag('div')->content(
+        'Height 75%'
+      )
+      ->w(75)
+      ->h(75)
+      ->bg('warning')
+      ->class('d-inline-block');
+
+    ?>
+
+    <div class="h-100 d-inline-block" style="width: 120px; background-color: rgba(0,0,255,.1)">Height 100%</div>
+    <?php
+      echo tag('div')->content(
+        'Height auto'
+      )
+      ->w(75)
+      ->h('auto')
+      ->bg('danger')
+      ->class('d-inline-block');
+
+    ?>
+  </div>    
+
+  <?php
 
   ?>
    
