e63a31a536867012d3b0a3143f5cb0eb526a8f5b
Pablo Bozzolo <boctulus@gmail.com>
2022-02-27 21:47:18 -0500

    .

--- app/views/rememberme.php
+++ app/views/rememberme.php
@@ -1,94 +1,51 @@
 <!-- Rememberme -->
     
-<style type="text/css">
-	.login-form {
-		width: 340px;
-    	margin: 30px auto;
-	}
-    .login-form form {
-    	margin-bottom: 15px;
-        background: #f7f7f7;
-        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-        padding: 30px;
-    }
-    .login-form h2 {
-        margin: 0 0 15px;
-    }
-    .login-form .hint-text {
-		color: #777;
-		padding-bottom: 15px;
-		text-align: center;
-    }
-    .form-control, .btn {
-        min-height: 50px;
-        border-radius: 2px;
-		font-size: 18px;
-    }
-    .login-btn {        
-        font-weight: bold;
-    }
-    .or-seperator {
-        margin: 20px 0 10px;
-        text-align: center;
-        border-top: 1px solid #ccc;
-    }
-    .or-seperator i {
-        padding: 0 10px;
-        background: #f7f7f7;
-        position: relative;
-        top: -11px;
-        z-index: 1;
-    }
-    .social-btn .btn {
-        margin: 12px 0;
-        font-size: 18px;
-        text-align: left; 
-        line-height: 40px;       
-    }
-	.social-btn .btn i {
-		float: left;
-		margin: 11px 15px  0 5px;
-        min-width: 15px;
-	}
-	.input-group-addon .fa{
-		font-size: 20px;
-	}
-</style>
+<?php
+    include_css(WIDGETS_PATH . 'login/login.css');
+?>
 
 <div class="row vh-100 d-flex  align-items-center">
 	<div class="col-xs-12 col-sm-6 offset-sm-3 col-md-4 offset-md-4">
-		<h1 style="font-size: 3em; padding-bottom: 0.5em;">Recuérdame</h1>
-
-        <div style="text-align:right; margin-bottom:1em;">
-            Tiene cuenta? <a href="login">Ingresar</a>
-        </div>
-
 		<?php
 
-		use simplerest\core\libs\HtmlBuilder\Bt5Form;
-		use simplerest\core\libs\HtmlBuilder\Tag;
+        use simplerest\core\libs\HtmlBuilder\Bt5Form;
+        use simplerest\core\libs\HtmlBuilder\Tag;
 
-		Tag::registerBuilder(\simplerest\core\libs\HtmlBuilder\Bt5Form::class);
+        Tag::registerBuilder(\simplerest\core\libs\HtmlBuilder\Bt5Form::class);
 
-		Bt5Form::setIdAsName();
+        Bt5Form::setIdAsName();
         
-        echo tag('inputGroup')->content([
-            Bt5Form::span('<i class="fas fa-envelope"></i>', [
-				'class' => 'input-group-text'
-			]),
+        echo tag('card')
+        ->header(tag('cardTitle')->text('Registro')->style('font-size: 300%;'))
+        ->body([
+            '<div style="text-align:right; margin-bottom:1em;">
+                Tiene cuenta? <a href="login">Ingresar</a>
+            </div>',
 
-            Bt5Form::email(
-				id:"email",
-				placeholder:"E-mail",
-				required:"required"
-			)
-        ])->class("input-group mb-3");		
-		?>		
+            tag('inputGroup')->content([
+                Bt5Form::span('<i class="fas fa-envelope"></i>', [
+                    'class' => 'input-group-text'
+                ]),
 
-		<div class="form-group mb-3">
-			<button type="submit" class="btn btn-primary btn-lg btn-block login-btn w-100" onClick="rememberme()">Recuérdame</button>
-        </div>
+                Bt5Form::email(
+                    id:"email",
+                    placeholder:"E-mail",
+                    required:"required"
+                )
+            ])->class("input-group mb-3"),		
+        
 
-        No registrado? <a href="login/register">regístrese</a>
+            '<div class="form-group mb-3">
+                <button type="submit" class="btn btn-primary btn-lg btn-block login-btn w-100" onClick="rememberme()">Recuérdame</button>
+            </div>
+
+            No registrado? <a href="login/register">regístrese</a>'
+            
+
+        ])->class('card-primary card-outline');
+        
+        ?>
+        
+        
 	</div>
 </div>
