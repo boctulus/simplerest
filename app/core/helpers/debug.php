af5425994722f6fdcaa2d39d9f9891bcc4efb86f
Pablo Bozzolo <boctulus@gmail.com>
2022-03-15 22:33:31 -0500

    .

--- app/core/helpers/debug.php
+++ app/core/helpers/debug.php
@@ -3,12 +3,12 @@
 use simplerest\core\libs\VarDump;
 
 if (!function_exists('dd')){
-    function dd(mixed $val = null, $msg = null, bool $additional_carriage_return = false){
+    function dd($val = null, $msg = null, bool $additional_carriage_return = false){
         return VarDump::dd($val, $msg, $additional_carriage_return);	
     }	
 }
 
-function d(mixed $val = null, $msg = null){
+function d($val = null, $msg = null){
     return VarDump::dd($val, $msg, true);	
 }
 
