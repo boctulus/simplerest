a29fa7242080df2efdcf5a299093f57a4550da1f
Pablo Bozzolo <boctulus@gmail.com>
2022-03-26 19:38:14 -0500

    .

---
+++ app/migrations/2022_03_26_44591114_ssl.php
@@ -0,0 +1,30 @@
+<?php
+
+use simplerest\core\interfaces\IMigration;
+use simplerest\core\libs\Factory;
+use simplerest\core\libs\Schema;
+use simplerest\core\Model;
+use simplerest\core\libs\DB;
+
+class Ssl implements IMigration
+{
+    /**
+	* Run migration.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        $sc = new Schema('ssl');
+
+        $sc
+        ->integer('id')->auto()->pri()
+        ->varchar('dominio', 50)
+        ->datetime('created_at')
+        ->datetime('expires_at')
+        ->datetime('updated_at')->nullable()
+
+		->create();		
+    }
+}
+
