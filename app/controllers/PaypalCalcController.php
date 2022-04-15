967ac9063a2a9d7fa42ed97c9395819b42533313
Pablo Bozzolo <boctulus@gmail.com>
2022-04-15 08:19:37 -0500

    .

---
+++ app/controllers/PaypalCalcController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace simplerest\controllers;
+
+use simplerest\controllers\MyController;
+use simplerest\core\Request;
+use simplerest\core\Response;
+use simplerest\core\libs\Factory;
+use simplerest\core\libs\DB;
+
+class PaypalCalcController extends MyController
+{
+    /*  
+        https://www.paypal.com/us/webapps/mpp/paypal-fees
+    */
+
+    static protected $per   = 5.5;
+    static protected $fixed = 0.3;
+
+    function __construct()
+    {
+        parent::__construct();        
+    }
+
+    /*
+        @param cantidad enviada
+        @return fees 
+    */
+    function fee($amount){
+        return static::$fixed + ($amount * static::$per * 0.01);
+    }
+
+    /*
+        @param cantidad que deseo recibir
+        @return cantidad que se debe enviar
+    */
+    function get($amount){
+        return $amount + $this->fee($amount);
+    }
+
+    /*
+        @param  cantidad a enviar
+        @return cantidad recibida
+    */
+    function send($amount){
+        return $amount - $this->fee($amount);
+    }
+
+    function index(){
+    }
+}
+
