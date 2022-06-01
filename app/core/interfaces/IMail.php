aff8647bc51f34e53bd8c496430b1cc87a67ea96
boctulus <boctulus@gmail.com>
2022-05-31 20:59:04 -0500

    .

--- app/core/interfaces/IMail.php
+++ app/core/interfaces/IMail.php
@@ -4,6 +4,6 @@ namespace simplerest\core\interfaces;
 
 interface IMail {
     
-    static function send(Array $to, $subject = '', $body = '', $alt_body = null, $attachments = null, Array $from = [], Array $cc = [], Array $bcc = [], Array $reply_to = []);
+    static function send(Array $to, $subject = '', $body = '', $attachments = null, Array $from = [], Array $cc = [], Array $bcc = [], Array $reply_to = [], $alt_body = null);
 
 }
