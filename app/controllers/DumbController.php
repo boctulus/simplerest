ec9f1c92c7ee81323614cc3f3485d4b8bce93b16
boctulus <boctulus@gmail.com>
2022-06-09 19:45:46 -0500

    .

--- app/controllers/DumbController.php
+++ app/controllers/DumbController.php
@@ -9763,18 +9763,6 @@ class DumbController extends Controller
     function fix_csv(){
         $out = [];
 
-
-        // Files::varExport(UPLOADS_PATH . 'test.php', [
-        //     'x' => 'Z'
-        // ]);
-
-        Files::JSONExport(UPLOADS_PATH . 'test.json', [
-            'x' => 'Z'
-        ]);
-
-
-        exit;
-
         $path = 'D:\Desktop\CSV\completo.csv';
 
         $rows = Files::getCSV($path)['rows'];
@@ -9791,9 +9779,11 @@ class DumbController extends Controller
             $out[$sku]['precio']      = $precio;
             $out[$sku]['precio_plus'] = $precio_plus;
         }
-
-        dd($out);
-        dd(count($out), 'COUNT');      
+        
+        Files::varExport(UPLOADS_PATH . 'completo-csv.php', $out);
+    
+        // dd($out);
+        // dd(count($out), 'COUNT');      
     }
 
 
