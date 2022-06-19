f66cce35bdc571b90111ab2c18dfe0492383df1b
boctulus <boctulus@gmail.com>
2022-06-18 20:39:20 -0500

    .

--- app/views/select2/test.php
+++ app/views/select2/test.php
@@ -1,30 +1,43 @@
 <script>
 
-  // General
-  function setDropdownOptions(select_elem, options, default_option){
-    select_elem.innerHTML = '';
+  function setDropdownOptionsSelect2(select_elem, options, default_option){
+    // clear
+    jQuery(select_elem).val(null).trigger('change');
+    jQuery(select_elem).val(null).empty().select2('destroy')
+    jQuery(select_elem).val('').trigger('change')
+    jQuery(select_elem).select2({data: {id:null, text: null}})
 
-    let opt = new Option(default_option['text'], default_option['value']);
-    opt.setAttribute('selected', true);
-    select_elem.appendChild(opt);
-
-    if (typeof options == 'undefined' || options == null || options.length == 0){
-      select_elem.disabled = true;
-      return;
-    } else {
-      select_elem.disabled = false;
-    }
-
-    for (let i=0; i<options.length; i++){
-      let opt = new Option(options[i]['text'], options[i]['value']);
-      select_elem.appendChild(opt);
-    }
+    jQuery(select_elem).select2({data: options});
   }
 
   let countries_elem;
+  let states_elem;
   
   let country_items = [];
   let state_items = [];
+
+  /*
+    Genera valores para rellenar el select
+  */
+  function fill_countries(){      
+    for (var i=0; i<countries.length; i++)
+    {
+      country_items.push({
+        'id': countries[i],
+        'text': countries[i]
+      });
+    }
+  }
+
+  function fill_states(states){      
+    for (var i=0; i<states.length; i++)
+    {
+      state_items.push({
+        'id': states[i],
+        'text': states[i]
+      });
+    }
+  }
 
 
   $(document).ready(function() {
@@ -32,10 +45,23 @@
       $('.select2-states').select2();
 
       countries_elem = document.getElementById('countries');
+      states_elem    = document.getElementById('states');
 
       fill_countries()
+      setDropdownOptionsSelect2(countries_elem, country_items, {'text': 'Pais', 'value': ''});
 
-      setDropdownOptions(countries_elem, country_items, {'text': 'Pais', 'value': ''});
+      jQuery(countries_elem).change(function(){
+        let country_name_selected = countries_elem.value;
+        
+        let state_name_selected = find_country(country_name_selected);
+
+        if (state_name_selected != null){
+          state_items = [];
+          fill_states(state_name_selected)
+          setDropdownOptionsSelect2(states_elem, state_items, {'text': 'Provincia o estado', 'value': ''});
+        }
+
+      });
   });
 
   const arr = <?= $json ?>
@@ -44,16 +70,20 @@
       return array.map(item => item[column]);
   };
 
-  const countries    = array_column(arr['countries'], 'country');
+  const countries = array_column(arr['countries'], 'country');
 
-  function fill_countries(){      
-      for (var i=0; i<countries.length; i++)
-      {
-        country_items.push({
-          'text': countries[i],
-          'value': countries[i]
-        });
+  /*
+    Busca por pais y devuelve los estados
+  */
+  function find_country(name){
+    for (var i=0; i<arr['countries'].length; i++)
+    {
+      if (arr['countries'][i]['country'] == name){
+          return arr['countries'][i]['states'];
+      }
     }
+
+    return null;
   }
 
 
@@ -68,7 +98,7 @@
 
 <p></p>
 
-<select class="select2-states" name="states[]" style="width:300px">
+<select class="select2-states" name="states[]" id="states" style="width:300px">
   <option value="">Provincia / estado</option>
 </select>
 
