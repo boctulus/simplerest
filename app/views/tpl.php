eea0129f197694f488b88e7ca96d49b662a1ac72
boctulus <boctulus@gmail.com>
2022-06-18 20:39:20 -0500

    .

--- app/views/tpl.php
+++ app/views/tpl.php
@@ -79,7 +79,7 @@
 
 
     <!-- Select2 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.3/dist/css/select2.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.2.0/dist/select2-bootstrap-5-theme.min.css" />
 
     <!-- DualListbox -->
@@ -140,7 +140,7 @@
     <script src="<?= asset('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') ?>"></script>
 
     <!-- Select2 -->
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.3/dist/js/select2.min.js"></script>
 
     
     <script src="<?= asset('js/boostrap_notices.js') ?>"></script>
