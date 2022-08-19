4a37f4372ddc541ca7562a19843173d79f06cdb7
boctulus <boctulus@gmail.com>
2022-08-19 13:12:57 +0200

    .

---
+++ app/controllers/Dumb2Controller.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace simplerest\controllers;
+
+use simplerest\controllers\MyController;
+use simplerest\core\Request;
+use simplerest\core\Response;
+use simplerest\core\libs\Factory;
+use simplerest\core\libs\Strings;
+
+class Dumb2Controller extends MyController
+{
+    function index()
+    {
+        $str = "		array (
+            'ID_COC' => 
+            array (
+              'type' => 'int',
+              'min' => 0,
+            ),
+            'COC_NOMBRE' => 
+            array (
+              'type' => 'str',
+              'max' => 60,
+              'required' => true,
+            ),
+            'COC_BORRADO' => 
+            array (
+              'type' => 'bool',
+            ),
+            'created_at' => 
+            array (
+              'type' => 'timestamp',
+            ),
+            'updated_at' => 
+            array (
+              'type' => 'timestamp',
+            ),
+          );";
+          
+          
+          echo Strings::trimMultiline($str). PHP_EOL;
+    }
+}
+
