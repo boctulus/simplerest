58562ad24a217adbf888f3d7797602a662cf80e0
boctulus <boctulus@gmail.com>
2022-08-22 22:50:40 +0200

    .

--- app/etc/templates/seeder.php
+++ app/etc/templates/seeder.php
@@ -4,12 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Database\Factories\GeneroFakerFactory;
 
 class __MODEL_NAME__Seeder extends Seeder
 {
-    static protected $qty = 5;
-
     /**
      * Run the database seeds.
      *
@@ -17,10 +14,11 @@ class __MODEL_NAME__Seeder extends Seeder
      */
     public function run()
     {
-        $seeder = new _MODEL_NAME__Factory();
-        for($i=0; $i<static::$qty; $i++){
-            $row = $seeder->definition();
-            DB::table('__TABLE_NAME__')->insertOrIgnore($row);
-        }        
+        $data = [
+            __DATA__
+        ];
+
+        \App\Models\__MODEL_NAME__::insertOrIgnore($data);
     }
 }
+
