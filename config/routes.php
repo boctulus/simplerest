a3f98a201b5565b3dff6cf1daca95a5ddc291d24
boctulus <boctulus@gmail.com>
2022-10-03 14:27:59 +0100

    .

--- config/routes.php
+++ config/routes.php
@@ -11,8 +11,20 @@ Route::get('hello', function(){
 
 Route::get('api/v1/cool',  'DumbAuthController@super_cool_action');
 
+/*
+	Actualmente el orden es importante...... 
+	... que debe corregirse.
+
+	Esta obligandose a ir de lo especifico a lo general
+*/
+
 Route::get('admin/una-pagina', function(){
-	$content = "Pagina de acceso restringido";
+	$content = "Pagina (de acceso restringido)";
+	render($content);
+});
+
+Route::get('admin/pagina-dos', function(){
+	$content = "Pagina dos (de acceso restringido)";
 	render($content);
 });
 
