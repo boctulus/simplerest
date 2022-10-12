34eb8163279ca63dfa9b9b6deb400cf03817d65c
boctulus <boctulus@gmail.com>
2022-10-12 23:18:20 +0100

    .

---
+++ app/core/templates/Middleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace simplerest\middlewares;
+
+use simplerest\core\Middleware;
+use simplerest\core\libs\DB;
+use simplerest\core\libs\Strings;
+
+class __NAME__ extends Middleware
+{   
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function handle(?callable $next = null){
+        $res = $this->res->get();
+
+        // ...
+
+        $this->res->set($res);
+    }
+}
