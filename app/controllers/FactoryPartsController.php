f15a41800e6756e7b9a5df2105dcbf0cd680b293
boctulus <boctulus@gmail.com>
2022-11-16 20:29:26 +0000

    .

--- app/controllers/FactoryPartsController.php
+++ app/controllers/FactoryPartsController.php
@@ -19,8 +19,16 @@ class FactoryPartsController extends MyController
     {   
         css_file('https://cdn.datatables.net/v/dt/jqc-1.12.4/dt-1.13.1/b-2.3.2/sl-1.5.0/datatables.min.css');
 		css_file(VIEWS_PATH . 'factory_parts/css/editor.dataTables.min.css');
-
-        js_file('https://cdn.datatables.net/v/dt/jqc-1.12.4/dt-1.13.1/b-2.3.2/sl-1.5.0/datatables.min.js');
+        css_file('https://cdn.datatables.net/buttons/1.1.2/css/buttons.dataTables.min.css');
+        css_file('https://cdn.datatables.net/select/1.1.2/css/select.dataTables.min.css');
+        css_file("https://cdn.datatables.net/responsive/2.0.2/css/responsive.dataTables.min.css");
+        
+        //js_file('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js');
+        js_file('https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js');
+        js_file('https://cdn.datatables.net/buttons/1.1.2/js/dataTables.buttons.min.js');
+        js_file('https://cdn.datatables.net/select/1.1.2/js/dataTables.select.min.js');
+        js_file('https://cdn.datatables.net/responsive/2.0.2/js/dataTables.responsive.min.js');
+        js_file('http://kingkode.com/datatables.editor.lite/js/altEditor/dataTables.altEditor.free.js');
 
         js_file(VIEWS_PATH . 'factory_parts/js/custom_dt.js');
 
--- app/views/factory_parts/js/custom_dt.js
+++ app/views/factory_parts/js/custom_dt.js
@@ -1,10 +1,45 @@
+/*
 
+
+    https://stackoverflow.com/questions/57321196/how-to-add-an-edit-buttons-in-datatables-plugin
+
+*/
 
 $(document).ready(function() {
-    $('#parts_tb').DataTable();
-} );
+    var data_use = [
+        ["Garrett Winters", "Accountant", "Tokyo", "8422"],
+        ["Ashton Cox", "Junior Technical Author", "San Francisco", "1562"],
+        ["Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224"],
+        ["Airi Satou", "Accountant", "Tokyo", "5407"],
+    ];
 
+    var column_name = [{
+        title: "table_1"
+    }, {
+        title: "table2"
+    }, {
+        title: "table3"
+    }, {
+        title: "table4"
+    }];
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    
+    $('#parts_tb').DataTable({
+        "sPaginationType" : "full_numbers",
+        data : data_use,
+        //columns : column_name,
+        dom : 'Bfrtip',
+        select : 'single',
+        responsive : true,
+        altEditor : true,
+        //destroy : true,
+        searching: false,
+        buttons : [
+            {
+                extend : 'selected',
+                text : 'Edit',
+                name : 'edit'
+            }
+        ],
+        
+    });
 });
