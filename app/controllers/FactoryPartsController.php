4a345262270167513b1da851f372964d57095df1
boctulus <boctulus@gmail.com>
2022-11-16 18:24:07 +0000

    .

--- app/controllers/FactoryPartsController.php
+++ app/controllers/FactoryPartsController.php
@@ -3,10 +3,10 @@
 namespace simplerest\controllers;
 
 use simplerest\controllers\MyController;
-use simplerest\core\Request;
-use simplerest\core\Response;
-use simplerest\core\libs\Factory;
-use simplerest\core\libs\DB;
+use
+    DataTables\Database,
+    DataTables\Editor,
+    DataTables\Editor\Field;
 
 class FactoryPartsController extends MyController
 {
@@ -17,21 +17,13 @@ class FactoryPartsController extends MyController
 
     function index()
     {   
-        css_file('https://cdn.datatables.net/v/dt/jqc-1.12.4/moment-2.18.1/dt-1.13.1/b-2.3.3/date-1.2.0/sl-1.5.0/datatables.min.css');
-		css_file(VIEWS_PATH . 'factory_parts/css/generator-base.css');
+        css_file('https://cdn.datatables.net/v/dt/jqc-1.12.4/dt-1.13.1/b-2.3.2/sl-1.5.0/datatables.min.css');
 		css_file(VIEWS_PATH . 'factory_parts/css/editor.dataTables.min.css');
 
-        js_file('https://cdn.datatables.net/v/dt/jqc-1.12.4/moment-2.18.1/dt-1.13.1/b-2.3.3/date-1.2.0/sl-1.5.0/datatables.min.js');
-        js_file('js/dataTables.editor.min.js');
-
-        // charset="utf-8"
-        js_file('js/table.part_numbers.js', [
-            'charset' => 'utf-8'
-        ]);
+        js_file('https://cdn.datatables.net/v/dt/jqc-1.12.4/dt-1.13.1/b-2.3.2/sl-1.5.0/datatables.min.js');
 
         js_file(VIEWS_PATH . 'factory_parts/js/custom_dt.js');
-        
-        
+
         view('factory_parts/parts');
     }
 }
--- app/views/factory_parts/js/custom_dt.js
+++ app/views/factory_parts/js/custom_dt.js
@@ -1,67 +1,10 @@
 
-/*
- * Editor client script for DB table part_numbers
- * Created by http://editor.datatables.net/generator
- */
+
+$(document).ready(function() {
+    $('#parts_tb').DataTable();
+} );
+
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    (function($){
-
-        $(document).ready(function() {
-            var editor = new $.fn.dataTable.Editor( {
-                ajax: 'php/table.part_numbers.php',
-                table: '#part_numbers',
-                fields: [
-                    {
-                        "label": "nombre:",
-                        "name": "nombre"
-                    },
-                    {
-                        "label": "nota:",
-                        "name": "nota"
-                    },
-                    {
-                        "label": "created_at:",
-                        "name": "created_at",
-                        "type": "datetime",
-                        "format": "ddd, D MMM YY"
-                    },
-                    {
-                        "label": "updated_at:",
-                        "name": "updated_at",
-                        "type": "datetime",
-                        "format": "ddd, D MMM YY"
-                    }
-                ]
-            } );
-        
-            var table = $('#part_numbers').DataTable( {
-                dom: 'Bfrtip',
-                ajax: 'php/table.part_numbers.php',
-                columns: [
-                    {
-                        "data": "nombre"
-                    },
-                    {
-                        "data": "nota"
-                    },
-                    {
-                        "data": "created_at"
-                    },
-                    {
-                        "data": "updated_at"
-                    }
-                ],
-                select: true,
-                lengthChange: false,
-                buttons: [
-                    { extend: 'create', editor: editor },
-                    { extend: 'edit',   editor: editor },
-                    { extend: 'remove', editor: editor }
-                ]
-            } );
-        } );
-        
-    }(jQuery));
     
 });
