d867526fa8d62a35ceff5f7dd5d607fb08d9b164
boctulus <boctulus@gmail.com>
2022-11-17 11:45:01 +0000

    .

--- app/middlewares/InyectarUsername.php
+++ app/middlewares/InyectarUsername.php
@@ -2,14 +2,24 @@
 
 namespace simplerest\middlewares;
 
-use simplerest\core\Middleware;
 use simplerest\core\libs\DB;
+use simplerest\core\libs\Files;
+use simplerest\core\Middleware;
 use simplerest\core\libs\Strings;
 
 class InyectarUsername extends Middleware
 {  
     function handle(?callable $next = null){
         $res = $this->res->get();
+
+        /*
+            Se espera el resultado pueda ser un JSON en este caso
+            ya que el Middleware se encuentra ya con la respuesta encodeada
+        */
+
+        if (is_string($res)){
+            $res = json_decode($res, true);
+        }
 
         $uid   = $res['data']['uid'];
         
