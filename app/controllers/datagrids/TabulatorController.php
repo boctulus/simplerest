b0470509d6edee2afced72f0c02d2d2b6fb93c54
boctulus <boctulus@gmail.com>
2022-11-17 11:45:01 +0000

    .

---
+++ app/controllers/datagrids/TabulatorController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace simplerest\controllers\datagrids;
+
+use simplerest\controllers\MyController;
+use simplerest\core\Request;
+use simplerest\core\Response;
+use simplerest\core\libs\Factory;
+use simplerest\core\libs\DB;
+
+class TabulatorController extends MyController
+{
+    function __construct()
+    {
+        parent::__construct();        
+    }
+
+    function index()
+    {
+        css_file('vendors/tabulator/dist/css/tabulator.min.css');
+		
+        js_file('vendors/tabulator/dist/js/tabulator.min.js');
+
+        view('factory_parts/parts');
+    }                
+
+}
+
