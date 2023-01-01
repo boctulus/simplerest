9995f036256108bb439b2cbb676cc7cd57f72f03
boctulus <boctulus@gmail.com>
2023-01-01 11:46:55 +0800

    .

---
+++ app/controllers/html_builder/DemoController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace simplerest\controllers\html_builder;
+
+use simplerest\core\controllers\Controller;
+use simplerest\core\libs\Time;
+
+class DemoController extends Controller
+{
+    /*
+        De momento NO usar cache !!!
+    */
+
+    function __construct()
+    {
+        js_file(ASSETS_PATH . 'js/popper.min.js', null, true);
+    }
+
+    function index(){
+        view('html_builder_examples/hello2');
+    }
+
+    function lte(){
+        view('html_builder_examples/lte');
+    }
+    
+    function test(){
+        view('html_builder_examples/test');
+    }
+
+    function test_multiple(){
+        view('html_builder_examples/multiple');
+    }
+   
+}
+
