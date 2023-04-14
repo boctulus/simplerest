5b4bbe420e1e287c00c0e21b595ef9eaa3a80417
Pablo Bozzolo <boctulus@gmail.com>
2023-04-14 13:01:23 +0800

    .

---
+++ app/core/libs/Xml.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace simplerest\core\libs;
+
+class Xml
+{
+    static function toArray(string $xml){
+        $xml = trim($xml);
+
+        libxml_use_internal_errors(true);
+
+        if (substr($xml, 0, 1) != '<'){
+            if (!file_exists($xml)){
+                throw new \InvalidArgumentException("File '$xml' not found");
+            }    
+
+            $objXmlDocument = simplexml_load_file($xml);
+        } else {
+            $objXmlDocument = simplexml_load_string($xml);
+        }
+
+        if ($objXmlDocument === false) {
+            $msg = "There were errors parsing the XML file.\n";
+            
+            $errors = [];
+            foreach(libxml_get_errors() as $error) {
+                $errors[] = $error->message;
+            }
+
+            throw new \Exception($msg . implode('. ', $errors));
+        }
+
+        $objJsonDocument = json_encode($objXmlDocument);
+        $arrOutput       = json_decode($objJsonDocument, true);
+        
+        libxml_use_internal_errors(false);
+
+        return $arrOutput;
+    }  
+    
+   
+
+}
+
