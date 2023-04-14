2631fab067e06c819e735afe70c591df20aa4ebf
Pablo Bozzolo <boctulus@gmail.com>
2023-04-14 13:03:38 +0800

    .

--- app/core/libs/Xml.php
+++ app/core/libs/Xml.php
@@ -38,7 +38,21 @@ class XML
         return $arrOutput;
     }  
     
-   
+    static function getDomDocument(string $html){
+        $doc = new \DOMDocument();
 
+        libxml_use_internal_errors(true);
+        $doc->loadHTML('<?xml encoding="UTF-8">' . $html);
+        libxml_use_internal_errors(false);
+
+        return $doc;
+    }  
+    
+    static function getXPath(string $html){
+        return new \DOMXPath(
+            static::getDomDocument($html)
+        );
+    }   
+   
 }
 
