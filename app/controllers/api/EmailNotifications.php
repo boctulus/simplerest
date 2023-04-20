8838e412b02ccd3bc668db715761485e2ec2047f
Pablo Bozzolo <boctulus@gmail.com>
2023-04-20 08:36:33 +0800

    .

--- app/controllers/api/EmailNotifications.php
+++ app/controllers/api/EmailNotifications.php
@@ -2,6 +2,8 @@
 
 namespace simplerest\controllers\api;
 
+use simplerest\core\libs\Mail;
+use simplerest\core\libs\Logger;
 use simplerest\controllers\MyApiController; 
 
 class EmailNotifications extends MyApiController
@@ -14,5 +16,30 @@ class EmailNotifications extends MyApiController
     function __construct()
     {       
         parent::__construct();
-    }        
+    }    
+    
+    /*
+		Si lo hago acá será síncrono (mala idea)
+	*/
+	// function onCreated(array &$data, $last_inserted_id)
+	// {
+	// 	// podría ser un CASE
+	// 	if ($data['subject'] == 'Cambio de contraseña'){
+	// 		$u_model_name = get_user_model_name();
+	// 		$u = new $u_model_name();
+
+	// 		$username = $u
+	// 		->where([
+	// 			$u_model_name::$email => $data['to_addr'] 
+	// 		])
+	// 		->value($u_model_name::$username);
+
+	// 		if ($username == false){
+	// 			$username = 'Usuario';
+	// 			Logger::log("Username para {$data['to_addr']} es indefinido !");
+	// 		}
+
+	// 		Mail::send($data['to_addr'], $username, $data['subject'], $data['body']);
+	// 	}
+	// }
 } 
