9646af19e049a39a1f26807460e665d41d5639e3
Pablo Bozzolo <boctulus@gmail.com>
2023-05-23 17:13:55 +0800

    .

--- app/core/libs/Cache.php
+++ app/core/libs/Cache.php
@@ -7,6 +7,28 @@ class Cache
     const FOREVER = -1;
     const EXPIRED =  0;
 
+    static function getCachePath(string $url) : string {
+        static $path;
+
+        if (isset($path[$url])){
+            return $path[$url];
+        }
+
+        $filename = str_replace(['%'], ['p'], urlencode(Url::normalize($url))) . '.php';
+        $filename = str_replace('/', '', $filename);
+
+        // Evito problemas con nombres largos
+        if (strlen($filename) > 250){
+            return null;
+        }
+
+        $path[$url] = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $filename;
+        return $path[$url];
+    }
+
+    /*
+        Logica para saber si un recurso ha expirado
+    */
     static function expired(int $cached_at, int $expiration_time) : bool {
         if ($expiration_time == 0){
             return true;
@@ -19,6 +41,9 @@ class Cache
         return time() > $cached_at + $expiration_time;;
     }
 
+    /*
+        Logica para saber si un archivo usado como cache ha expirado
+    */
     static function expiredFile(string $cache_path, int $expiration_time) : bool {
         $exists = file_exists($cache_path);
 
