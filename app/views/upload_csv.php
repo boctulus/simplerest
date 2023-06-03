f3c3a541f7b9063a77cd5a409c066cf16298805d
Pablo Bozzolo <boctulus@gmail.com>
2023-06-04 00:03:31 +0800

    .

--- app/views/upload_csv.php
+++ app/views/upload_csv.php
@@ -16,6 +16,23 @@
 
     csvFileInput.addEventListener('change', handleFileSelect, false);
 
+    function convertToAssociativeArray(headers, rows) {
+        const arrayOfObjects = [];
+
+        rows.forEach(row => {
+            const obj = {};
+
+            row.forEach((value, index) => {
+                const header = headers[index];
+                obj[header] = value;
+            });
+
+            arrayOfObjects.push(obj);
+        });
+
+        return arrayOfObjects;
+    }
+
     function handleFileSelect(event) {
         const file   = event.target.files[0];
         const reader = new FileReader();
@@ -24,8 +41,11 @@
             const csvContent = e.target.result;
             
             const parsedData = processCSVContent(csvContent);
-            console.log(parsedData.headers); // ["Header 1", "Header 2", "Header 3"]
-            console.log(parsedData.data); // [["Value 1", "Value 2", "Value 3"]]
+            // console.log(parsedData.headers); // ["Header 1", "Header 2", "Header 3"]
+            // console.log(parsedData.rows);    // [["Value 1", "Value 2", "Value 3"]]
+
+            const associativeArray = convertToAssociativeArray(parsedData.headers, parsedData.rows);
+            console.log(associativeArray);   // { "Header 1": "Value 1", "Header 2": "Value 2", "Header 3": "Value 3" }
         };
 
         reader.readAsText(file);
@@ -34,12 +54,15 @@
     function processCSVContent(csvContent) {
         const results = Papa.parse(csvContent);
         const data = results.data;
-        const lines = data.slice(1).map(line => line.toString().split(','));
+        const lines = data.slice(1).map(line => line.map(value => value.trim()));
         const headers = data[0].map(header => header.replace(/\t/g, ''));
+
+        // Eliminar filas con un solo campo vacío
+        const filteredLines = lines.filter(line => line.some(value => value !== ''));
 
         return {
             headers: headers,
-            data: lines
+            rows: filteredLines
         };
     }
 
