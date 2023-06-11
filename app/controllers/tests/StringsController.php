a68f4562cb845847ccc560fd6fe1de031bca2945
Pablo Bozzolo <boctulus@gmail.com>
2023-06-11 20:51:02 +0800

    .

--- app/controllers/tests/StringsController.php
+++ app/controllers/tests/StringsController.php
@@ -23,13 +23,16 @@ class StringsController extends MyController
 
     // ok
     function test_get_up_to() {
-        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 10));
-        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 17));
-        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 18));
-        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 19));
-        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 3));
-        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", null, 12));
-        dd(Strings::getUpTo("Esta es una prueba de método getUpTo"));
+        dd(Strings::getUpTo("MAYORISTA CSV Impor", PHP_INT_MAX, 15)); // MAYORISTA CSV
+        dd(Strings::getUpTo("MAYORISTA CSV Impor", null, 15)); // MAYORISTA CSV I
+
+        // dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 10));
+        // dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 17));
+        // dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 18));
+        // dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 19));
+        // dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 3));
+        // dd(Strings::getUpTo("Esta es una prueba de método getUpTo", null, 12));
+        // dd(Strings::getUpTo("Esta es una prueba de método getUpTo"));
     }
 
     // ...
