b3929a80f4ae8d2ca1da8df041af9c029a57e01a
Pablo Bozzolo <boctulus@gmail.com>
2023-06-11 20:22:41 +0800

    .

---
+++ app/controllers/tests/StringsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace simplerest\controllers\tests;
+
+use simplerest\core\libs\Strings;
+use simplerest\controllers\MyController;
+
+/*
+    Ajustar nomenclatura a esta misma test_{nombre de metodo snake-case}
+    y... convertir en pruebas unitarias
+*/
+class StringsController extends MyController
+{
+    // ok
+    function test_to_snake_case(){
+        return Strings::toSnakeCase('MAYORISTA CSV Import');
+    }
+
+    // getUpToNWords
+    function test_get_up_to_n_words(){
+        return Strings::getUpToNWords('El dia que me quieras', 3);
+    }
+
+    // ok
+    function test_get_up_to() {
+        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 10));
+        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 17));
+        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 18));
+        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 4, 19));
+        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", 3));
+        dd(Strings::getUpTo("Esta es una prueba de método getUpTo", null, 12));
+        dd(Strings::getUpTo("Esta es una prueba de método getUpTo"));
+    }
+
+    // ...
+}
+
