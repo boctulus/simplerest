a46a63f619e573470e5a0516a59e208905fb09ef
Pablo Bozzolo <boctulus@gmail.com>
2023-06-23 01:23:30 +0800

    .

--- app/core/libs/Zip.php
+++ app/core/libs/Zip.php
@@ -13,37 +13,43 @@ class Zip
         return $return_var === 0;
     }
 
-    static function unzip(string $file_path, $destination = null) {
+    public static function unzip(string $file_path, $destination = null) {
+        // Utilizar la ruta de destino si se proporciona
+        if ($destination !== null) {
+            $destination_folder = rtrim($destination, '/');
+        } else {
+            $destination_folder = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid('unzip_');
+        }
+
         // Verificar si la extensión ZipArchive está disponible
         if (extension_loaded('zip')) {
             $zip = new \ZipArchive();
             
             // Abrir el archivo zip
             if ($zip->open($file_path) === true) {
-                // Extraer los archivos en una carpeta temporal
-                $temp_folder = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid('unzip_');
-                $zip->extractTo($temp_folder);
+                // Extraer los archivos en la carpeta de destino
+                $zip->extractTo($destination_folder);
                 $zip->close();
                 
-                // Retornar la ruta de la carpeta temporal
-                return $temp_folder;
+                // Retornar la ruta de la carpeta de destino
+                return $destination_folder;
             }
         }
         
         // Verificar si el comando unzip está disponible
         if (self::isUnzipCommandAvailable()) {
             // Ejecutar el comando unzip
-            $destination = sys_get_temp_dir() . '/' . uniqid('unzip_');
-            $command = "unzip $file_path -d $destination";
+            $command = "unzip $file_path -d $destination_folder";
             exec($command);
             
             // Retornar la ruta de la carpeta de destino
-            return $destination;
+            return $destination_folder;
         }
         
         // Lanzar excepción si ninguna opción está disponible
         throw new \Exception('No se puede descomprimir el archivo. El servicio no está disponible.');
     }
 }
+
 
 
