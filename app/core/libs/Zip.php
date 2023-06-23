0d5d02cbe04aafbec6d0fc9da7d95cdac8fafad4
Pablo Bozzolo <boctulus@gmail.com>
2023-06-23 23:14:47 +0800

    .

--- app/core/libs/Zip.php
+++ app/core/libs/Zip.php
@@ -5,77 +5,77 @@ namespace simplerest\core\libs;
 class Zip 
 {
     /*
-		https://stackoverflow.com/a/1334949/980631
+        https://stackoverflow.com/a/1334949/980631
 
-		Modified by @boctulus
+        Modified by @boctulus
 
         Por concistencia si no esta presente la extension, intentar usar el comando zip
-	*/
-	static function zip(string $ori, string $dst, ?Array $exclude = null, bool $overwrite = true)
-	{
-		if (!extension_loaded('zip') || !file_exists($ori)) {
-			return false;
-		}
-	
-		$zip = new \ZipArchive();
-		if (!$zip->open($dst, $overwrite && file_exists($dst) ? \ZipArchive::OVERWRITE : \ZipArchive::CREATE)) {
-			return false;
-		}
-	
-		if (is_null($exclude)){
-			$exclude = [];
-		}
+    */
+    static function zip(string $ori, string $dst, ?Array $except = null, bool $overwrite = true)
+    {
+        if (!extension_loaded('zip') || !file_exists($ori)) {
+            return false;
+        }
+    
+        $zip = new \ZipArchive();
+        if (!$zip->open($dst, $overwrite && file_exists($dst) ? \ZipArchive::OVERWRITE : \ZipArchive::CREATE)) {
+            return false;
+        }
+    
+        if (is_null($except)){
+            $except = [];
+        }
 
-		$ori = str_replace('\\', '/', realpath($ori));
-	
-		if (is_dir($ori) === true)
-		{
-			$new_excluded = [];
-			foreach ($exclude as $ix => $file){
-				if (!Files::isAbsolutePath($file)){
-					$exclude[$ix] = Files::getAbsolutePath($file, $ori);
-				}
+        $ori = str_replace('\\', '/', realpath($ori));
+    
+        if (is_dir($ori) === true)
+        {
+            $new_excluded = [];
+            foreach ($except as $ix => $file){
+                if (!Files::isAbsolutePath($file)){
+                    $except[$ix] = Files::getAbsolutePath($file, $ori);
+                }
 
-				if (is_dir($exclude[$ix])){
-					$new_excluded = array_merge($new_excluded, Files::recursiveGlob($exclude[$ix] . '/*'));	
-				}
-			}
+                if (is_dir($except[$ix])){
+                    $new_excluded = array_merge($new_excluded, Files::recursiveGlob($except[$ix] . '/*'));  
+                }
+            }
 
-			$exclude = array_merge(array_values($exclude), array_values($new_excluded));
+            $except = array_merge(array_values($except), array_values($new_excluded));
 
-			$files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($ori), \RecursiveIteratorIterator::SELF_FIRST);
-	
-			foreach ($files as $file)
-			{
-				$file = str_replace('\\', '/', $file);
-	
-				// Ignore "." and ".." folders
-				if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
-					continue;
-	
-				$file = realpath($file);
-	
-				if (!empty($exclude) && in_array($file, $exclude)){
-					continue;
-				}
+            $files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($ori), \RecursiveIteratorIterator::SELF_FIRST);
+    
+            foreach ($files as $file)
+            {
+                $file = str_replace('\\', '/', $file);
+    
+                // Ignore "." and ".." folders
+                if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
+                    continue;
+    
+                $file = realpath($file);
+    
+                if (!empty($except) && in_array($file, $except)){
+                    continue;
+                }
 
-				if (is_dir($file) === true && !in_array($file, $exclude))
-				{
-					$zip->addEmptyDir(str_replace($ori . '/', '', $file . '/'));
-				}
-				else if (is_file($file) === true)
-				{
-					$zip->addFromString(str_replace($ori . '/', '', $file), file_get_contents($file));
-				}
-			}
-		}
-		else if (is_file($ori) === true)
-		{
-			$zip->addFromString(basename($ori), file_get_contents($ori));
-		}
-	
-		return $zip->close();
-	}
+                if (is_dir($file) === true && !in_array($file, $except))
+                {
+                    $zip->addEmptyDir(str_replace($ori . '/', '', $file . '/'));
+                }
+                else if (is_file($file) === true)
+                {
+                    $zip->addFromString(str_replace($ori . '/', '', $file), file_get_contents($file));
+                }
+            }
+        }
+        else if (is_file($ori) === true)
+        {
+            $zip->addFromString(basename($ori), file_get_contents($ori));
+        }
+    
+        return $zip->close();
+    }
 
     protected static function isUnzipCommandAvailable() {
         // Verificar si el comando unzip está disponible en el sistema
@@ -86,10 +86,35 @@ class Zip
         return $return_var === 0;
     }
 
+    static function verifyUnzip(string $zipFile, string $destination)
+    {
+        $zip = new \ZipArchive();
+        if ($zip->open($zipFile) === true) {
+            $extractedFiles = [];
+
+            for ($i = 0; $i < $zip->numFiles; $i++) {
+                $extractedFiles[] = $zip->getNameIndex($i);
+            }
+
+            $zip->close();
+
+            foreach ($extractedFiles as $file) {
+                $extractedFilePath = $destination . '/' . $file;
+                if (!file_exists($extractedFilePath)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
     //
     // Por concistencia implementar $overwrite 
     //
-    public static function unzip(string $file_path, $destination = null) {
+    public static function unzip(string $file_path, $destination = null, bool $verify = true) {
         // Utilizar la ruta de destino si se proporciona
         if ($destination !== null) {
             $destination_folder = rtrim($destination, '/');
@@ -106,6 +131,10 @@ class Zip
                 // Extraer los archivos en la carpeta de destino
                 $zip->extractTo($destination_folder);
                 $zip->close();
+
+                if ($verify && !static::verifyUnzip($file_path, $destination)){
+                    throw new \Exception("Uncompression failled or finish with errors");                    
+                }
                 
                 // Retornar la ruta de la carpeta de destino
                 return $destination_folder;
@@ -117,6 +146,10 @@ class Zip
             // Ejecutar el comando unzip
             $command = "unzip $file_path -d $destination_folder";
             exec($command);
+
+            if ($verify && !static::verifyUnzip($file_path, $destination)){
+                throw new \Exception("Uncompression failled or finish with errors");                    
+            }
             
             // Retornar la ruta de la carpeta de destino
             return $destination_folder;
@@ -125,6 +158,8 @@ class Zip
         // Lanzar excepción si ninguna opción está disponible
         throw new \Exception('No se puede descomprimir el archivo. El servicio no está disponible.');
     }
+
+
 }
 
 
