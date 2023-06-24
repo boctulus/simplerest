6a75e482d019a75a0b2b1df8f0c4f08f6315592b
Pablo Bozzolo <boctulus@gmail.com>
2023-06-25 07:40:25 +0800

    .

--- app/core/libs/SimpleCrypt.php
+++ app/core/libs/SimpleCrypt.php
@@ -5,11 +5,22 @@ namespace simplerest\core\libs;
 /*
     Mejor que nada..... 
 
-    Ventaja: no utiliza extensiones de PHP que pueden no estar instaladas
+    - No utiliza extensiones de PHP que pueden no estar instaladas
+    - Puede mantener la longitud original
 */
 
-class SimpleCrypt {
-    public static function encrypt($str) {
+class SimpleCrypt 
+{
+    protected static $settings = [
+        'keep_length' => false
+    ];
+
+    /*
+        Encriptacion simetrica
+
+        crypt(crypt($str)) == $str
+    */
+    static function crypt($str) {
         $length = strlen($str);
         $encrypted = '';
         
@@ -18,19 +29,24 @@ class SimpleCrypt {
             $substring = substr($str, $i, 2);
             $encrypted .= strrev($substring); // Revertir subcadena
         }
-        
+      
         return $encrypted;
     }
 
-    public static function decrypt($str) {
-        $length = strlen($str);
-        $decrypted = '';
-        
-        // Revertir las translocaciones reversibles de subcadenas
-        for ($i = 0; $i < $length; $i += 2) {
-            $substring = substr($str, $i, 2);
-            $decrypted .= strrev($substring); // Revertir subcadena
+    static function encrypt($str) {
+        $encrypted = static::crypt($str);
+      
+        if (!static::$settings['keep_length']){
+            $encrypted = Strings::randomHexaString(7) . $encrypted . Strings::randomHexaString(13);
         }
+
+        return $encrypted;
+    }
+
+    static function decrypt($str) {
+        $str = substr($str, 7, strlen($str) - 20);
+
+        $decrypted = static::crypt($str);
         
         return $decrypted;
     }
