a48ee7e1d8ac9a0808326975c674d32c07270418
Pablo Bozzolo <boctulus@gmail.com>
2023-08-18 14:26:06 +0800

    .

--- app/controllers/CssExtractorController.php
+++ app/controllers/CssExtractorController.php
@@ -4,7 +4,7 @@ namespace simplerest\controllers;
 
 use simplerest\core\libs\DB;
 use simplerest\core\Request;
-use simplerest\core\libs\XML;
+use simplerest\core\libs\CSS;
 use simplerest\core\Response;
 use simplerest\core\libs\Files;
 use simplerest\core\libs\Factory;
@@ -17,7 +17,7 @@ class CssExtractorController extends MyController
         $html = Files::getContent('D:\www\simplerest\etc\practicatest\1.html');
 
         dd(
-            XML::extractLinkUrls($html, 'css')
+            CSS::extractLinkUrls($html, true)
         );
     }
 }
