bcb4ad4bbaa04c75f5c5d98ed9bbe3a47c2b828d
Pablo Bozzolo <boctulus@gmail.com>
2023-08-19 12:40:46 +0800

    .

--- app/controllers/CssExtractorController.php
+++ app/controllers/CssExtractorController.php
@@ -2,21 +2,20 @@
 
 namespace simplerest\controllers;
 
-use simplerest\core\libs\CSS;
-use simplerest\core\libs\Url;
-use simplerest\core\libs\XML;
-use simplerest\core\libs\Files;
 use simplerest\core\libs\Strings;
+use simplerest\core\libs\CSS;
+use simplerest\core\libs\Files;
+use simplerest\core\libs\Url;
 use simplerest\controllers\MyController;
 
 class CssExtractorController extends MyController
 {
     function parse()
     {
-        $html = Files::getContent('D:\www\simplerest\etc\practicatest\1.html');
+         $html = Files::getContent('D:\www\simplerest\etc\practicatest\1.html');
 
         dd(
-            XML::extractLinksByRelType($html, "stylesheet")
+            CSS::extractLinkUrls($html, true, true)
         );
 
         exit;
