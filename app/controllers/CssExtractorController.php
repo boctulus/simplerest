61553723af4fbb523f9278809e43278177ecf1d6
Pablo Bozzolo <boctulus@gmail.com>
2023-08-19 12:44:19 +0800

    .

--- app/controllers/CssExtractorController.php
+++ app/controllers/CssExtractorController.php
@@ -12,42 +12,26 @@ class CssExtractorController extends MyController
 {
     function parse()
     {
-         $html = Files::getContent('D:\www\simplerest\etc\practicatest\1.html');
-
-        dd(
-            CSS::extractLinkUrls($html, true, true)
-        );
-
-        exit;
-
-        /*
-            La idea es:
-
-            - Descargar cada archivo .css    -- ok
+        /*           
+            - Descargar cada archivo .css
             - Generar una linea con css_file() para cada uno
-            - Permitir excepciones o en algunos casos hacerlas de forma automatica como con Google Fonts
-
-            https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap
         */
 
-        // provendria de otra funcion
+        $html = Files::getContent('D:\www\simplerest\etc\practicatest\1.html');
 
-        $urls = [
-            'https://practicatest.cl/dist/css/basic.min.css',
-            'https://practicatest.cl/dist/css/style.themed.css?v=1'
-        ];
+        $urls = CSS::extractLinkUrls($html, true);
 
         $filenames = [];
         foreach ($urls as $url){
             $domain = Url::getDomain($url);
             $path   = ASSETS_PATH . $domain;
 
-            // Files::mkDirOrFail($path);        
-            // $bytes = Files::download($url, $path);   
+            Files::mkDirOrFail($path);        
+            $bytes = Files::download($url, $path);   
 
-            // if (empty($bytes)){
-            //     throw new \Exception("Download '$url' was not possible");
-            // }
+            if (empty($bytes)){
+                throw new \Exception("Download '$url' was not possible");
+            }
 
             $filename    = Files::getFilenameFromURL($url);
             $filenames[] = $path . DIRECTORY_SEPARATOR . $filename;
