94d09b63421242338efad45303036264da5049c1
Pablo Bozzolo <boctulus@gmail.com>
2023-09-22 22:04:57 +0800

    .

--- app/shortcodes/rack_quoter/views/racks.php
+++ app/shortcodes/rack_quoter/views/racks.php
@@ -1,95 +1,48 @@
-<?php
-
-  // $url = shortcode_asset(__DIR__ . '/images/WES-Logo.png');
-
-  // dd($url, 'URL');
-
-  // echo "<img src=\"$url\">";
-  // exit;
-?>
-
-<div class="clearfix text-center"><a href="http://warehouserack.com/" class="navbar-brand">
-    <img src="<?= shortcode_asset(__DIR__ . '/images/WES-Logo.png') ?>" alt="logo" style="max-height: 34px;"></a>
-    <h2>
-        Pallet Rack Layout Drawing</h2><!---->
-    <p id="palletsCount" class="subheading">
-        This Layout Will Store 36 Pallets</p><!---->
-</div>
-<div class="clearfix">
-    <div class="row">
-        <div class="col-md-12 col-xs-12 text-center">
-            <div class="-content">
-                <div class="main-img"><img
-                        data-ng-src="https://genie.warehouserack.com/api/drawing/preview/?ItemKey=96x36x96&amp;Condition=new&amp;Design=3&amp;Levels=2&amp;PlaceWidth=600&amp;PlaceLength=120&amp;Aisle=100&amp;ItemTypeKey=pallet-rack&amp;ItemGroupKey=&amp;UseWireDecking=false&amp;UsePalletSupport=false&amp;maxheight=0"
-                        src="https://genie.warehouserack.com/api/drawing/preview/?ItemKey=96x36x96&amp;Condition=new&amp;Design=3&amp;Levels=2&amp;PlaceWidth=600&amp;PlaceLength=120&amp;Aisle=100&amp;ItemTypeKey=pallet-rack&amp;ItemGroupKey=&amp;UseWireDecking=false&amp;UsePalletSupport=false&amp;maxheight=0">
-                </div><!---->
-                <div><!---->
-                    <p class="subheading">Redraw With Different Forklift</p><!---->
-                    <div class="-content alignment"><span class="primary-description">Click Below to view your space
-                            with
-                            a different aisle dimension</span><!---->
-                        <div aisle="controller.Model.Aisle">
-                            <div class="flex sb sm-c wrap aisle-wrapper">
-                                <div class="-item-inline">
-                                    <div class="-caption"><label for="itemCheck-4" class="check-default -mark -l"><input
-                                                type="radio" id="itemCheck-4" name="aisle" value="66"
-                                                class="ng-pristine "> <span>5' 6''
-                                                Aisle</span></label></div><!---->
-                                    <div class="-img"><img src="/assets/img/tab4-img04.png">
-                                        <h4>Drexel Forklift</h4>
-                                    </div><!---->
-                                </div>
-                                <div class="-item-inline">
-                                    <div class="-caption"><label for="itemCheck-3" class="check-default -mark -l"><input
-                                                type="radio" id="itemCheck-3" name="aisle" value="78"
-                                                class="ng-pristine "> <span>6' 6''
-                                                Aisle</span></label></div><!---->
-                                    <div class="-img"><img src="/assets/img/tab4-img03.png">
-                                        <h4>Bendi Forklift</h4>
-                                    </div><!---->
-                                </div>
-                                <div class="-item-inline">
-                                    <div class="-caption"><label for="itemCheck-2" class="check-default -mark -l"><input
-                                                type="radio" id="itemCheck-2" name="aisle" value="114"
-                                                class="ng-pristine "> <span>9' 6''
-                                                Aisle</span></label></div><!---->
-                                    <div class="-img"><img src="/assets/img/tab4-img02.png">
-                                        <h4>Reach Truck</h4>
-                                    </div><!---->
-                                </div>
-                                <div class="-item-inline">
-                                    <div class="-caption"><label for="itemCheck-6" class="check-default -mark -l"><input
-                                                type="radio" id="itemCheck-6" name="aisle" value="132"
-                                                class="ng-pristine "> <span>11'
-                                                Aisle</span></label></div><!---->
-                                    <div class="-img"><img src="/assets/img/tab5-img03.png">
-                                        <h4>3 wheel forklift</h4>
-                                    </div><!---->
-                                </div>
-                                <div class="-item-inline">
-                                    <div class="-caption"><label for="itemCheck-1" class="check-default -mark -l"><input
-                                                type="radio" id="itemCheck-1" name="aisle" value="156"
-                                                class="ng-pristine "> <span>13'
-                                                Aisle</span></label></div><!---->
-                                    <div class="-img"><img src="/assets/img/tab4-img01.png">
-                                        <h4>4 wheel forklift</h4>
-                                    </div><!---->
-                                </div>
-                            </div>
-                            <div class="clearfix aisle-wrapper">
-                                <div class="-item-inline">
-                                    <div class="form-group text-center"><label for="itemCheck-5"
-                                            class="check-default -mark -l"><input type="radio" id="itemCheck-5"
-                                                name="aisle" data-ng-checked="customeAisle" checked="checked"> <span
-                                                class="h4">Enter a custom aisle
-                                                dimension</span></label> <input type="text" class="form-control -small "
-                                            style="margin: auto;" placeholder="inches"></div>
-                                </div>
-                            </div>
-                        </div><!----><!---->
-                    </div>
-                </div><!---->
-            </div>
+<div class="-content alignment" style="margin-top:-35px;"><span class="primary-description" style="font-style: italic;">Click Below to view your space
+    with a different aisle dimension</span><!---->
+<div aisle="controller.Model.Aisle">
+    <div class="flex sb sm-c wrap aisle-wrapper">
+        <div class="-item-inline">
+            <div class="-caption"><label for="itemCheck-4" class="check-default -l"><input type="radio" id="itemCheck-4" name="aisle" value="66" class="ng-pristine "> <span>5' 6''
+                        Aisle</span></label></div><!---->
+            <div class="-img"><img src="http://woo7.lan/wp-content/plugins/rack_quoter/app/shortcodes/rack_quoter/assets/img/tab4-img04.png">
+                <h4>Drexel Forklift</h4>
+            </div><!---->
+        </div>
+        <div class="-item-inline">
+            <div class="-caption"><label for="itemCheck-3" class="check-default -l"><input type="radio" id="itemCheck-3" name="aisle" value="78" class="ng-pristine "> <span>6' 6''
+                        Aisle</span></label></div><!---->
+            <div class="-img"><img src="http://woo7.lan/wp-content/plugins/rack_quoter/app/shortcodes/rack_quoter/assets/img/tab4-img03.png ">
+                <h4>Bendi Forklift</h4>
+            </div><!---->
+        </div>
+        <div class="-item-inline">
+            <div class="-caption"><label for="itemCheck-2" class="check-default -l"><input type="radio" id="itemCheck-2" name="aisle" value="114" class="ng-pristine "> <span>9' 6''
+                        Aisle</span></label></div><!---->
+            <div class="-img"><img src="http://woo7.lan/wp-content/plugins/rack_quoter/app/shortcodes/rack_quoter/assets/img/tab4-img02.png">
+                <h4>Reach Truck</h4>
+            </div><!---->
+        </div>
+        <div class="-item-inline">
+            <div class="-caption"><label for="itemCheck-6" class="check-default -l"><input type="radio" id="itemCheck-6" name="aisle" value="132" class="ng-pristine "> <span>11'
+                        Aisle</span></label></div><!---->
+            <div class="-img"><img src="http://woo7.lan/wp-content/plugins/rack_quoter/app/shortcodes/rack_quoter/assets/img/tab4-img03.png">
+                <h4>3 wheel forklift</h4>
+            </div><!---->
+        </div>
+        <div class="-item-inline">
+            <div class="-caption"><label for="itemCheck-1" class="check-default -l"><input type="radio" id="itemCheck-1" name="aisle" value="156" class="ng-pristine "> <span>13'
+                        Aisle</span></label></div><!---->
+            <div class="-img"><img src="http://woo7.lan/wp-content/plugins/rack_quoter/app/shortcodes/rack_quoter/assets/img/tab4-img01.png">
+                <h4>4 wheel forklift</h4>
+            </div><!---->
         </div>
     </div>
+    <div class="clearfix aisle-wrapper">
+        <div class="-item-inline">
+            <div class="form-group text-center"><label for="itemCheck-5" class="check-default -l"><input type="radio" id="itemCheck-5" name="aisle" data-ng-checked="customeAisle" checked="checked"> <span class="h4">Enter a custom aisle
+                        dimension</span></label> <input type="text" class="form-control -small " style="margin: auto;" placeholder="inches"></div>
+        </div>
+    </div>
+</div><!----><!---->
 </div>
