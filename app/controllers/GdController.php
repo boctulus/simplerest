85fb8c9e284f8688091c97f70b580233dc443e84
Pablo Bozzolo <boctulus@gmail.com>
2023-09-23 21:11:40 +0800

    .

--- app/controllers/GdController.php
+++ app/controllers/GdController.php
@@ -61,17 +61,33 @@ class GdController extends MyController
         // Defino color defecto de pincel
         #$im->setForegroundColor('steelblue');
         
-        // Defino forma personalizada
-        $im->setShape('row', function($cells_per_row, $x1, $y1, $w, $h, $color = null, $x_sp = 0) use($im) {
+        /*
+            Defino formas personalizadas
+        */
+
+        $im->setShape('row', function($cells_per_row, $x1, $y1, $w, $h, $color = null, bool $filled = false, $x_sp) use($im) {
             if ($color == null){
                 $color = $im->getForegroundColor();
             }
 
             foreach (range(0, $cells_per_row-1) as $c ){
                 $x1 += $x_sp + $w;
-                $im->rectangle($x1, $y1, $w, $h, $color);       
+                $im->rectangle($x1, $y1, $w, $h, $color, $filled);       
             }
         });
+
+
+        /*
+            Ej:
+
+            $im->multipleRow(2, $boxes_per_row, $x, $y + 200, $w, $h, null, true, 2, 2);
+        */
+        $im->setShape('multipleRow', function($n, $cells_per_row, $x1, $y1, $w, $h, $color=null, bool $filled=false, $x_sp=0, $y_sp=0) use($im) {
+            foreach (range(0,$n-1) as $i){
+                $im->row($cells_per_row, $x1, $y1 + (($h + $y_sp) * $i), $w, $h, $color, $filled, $x_sp);
+            }
+        });        
+
 
         $boxes_per_row = 10;
 
@@ -80,16 +96,17 @@ class GdController extends MyController
         $w = 80;
         $h = 20;
 
+
         /*
             Ahora debo crear el arreglo de filas
+
+            Necesito:
+
+            - Saber los limites de la forma
+            - Conocer el punto medio en X y en Y 
         */
 
-        $im->setShape('multipleRow', function($n, $cells_per_row, $x1, $y1, $w, $h, $color = null, $x_sp = 0) use($im) {
-            foreach (range(0,$n-1) as $i){
-                $im->row($cells_per_row, $x1, $y1 + ($h * $i), $w, $h, $color, $x_sp);
-            }
-        });        
-
+   
         $im->multipleRow(1, $boxes_per_row, $x, $y,       $w, $h);
 
         $im->multipleRow(2, $boxes_per_row, $x, $y + 200, $w, $h);
@@ -97,12 +114,48 @@ class GdController extends MyController
         $im->multipleRow(1, $boxes_per_row, $x, $y + 400, $w, $h);
 
 
-
         // ...
 
         $im->render();                      
     }
    
+    /*
+        Voy a intentar simular "layers"
+    */
+    function render_02(){
+        $im_1 = new GdImage(1000, 1000);
+
+        $im_1->createColor('black', 0,0,0);
+        $im_1->createColor('white', 255,255,255);
+        $im_1->createColor('steelblue', 70,130,180);
+
+        $im_1->setBackgroundColor('white');
+        $im_1->invertColors();
+
+        $im_1->line(0, 0, 200, 200, 'steelblue');
+        $im_1->rectangle(50, 50, 100, 30, null, true);  // si se dibuja despues queda "arriba"       
+
+        $im_1->render();
+                    
+    }
+
+
+    function render_50(){
+        $redimg = imagecreatetruecolor(100, 100);
+        $image = imagecreatefrompng('image.png');
+
+        // sets background to red
+        $red = imagecolorallocate($redimg, 255, 0, 0);
+        imagefill($redimg, 0, 0, $red);
+
+        // Merge the red image onto the PNG image
+        imagecopymerge($image, $redimg, 0, 0, 0, 0, 100, 100, 75);
+
+        header('Content-type: image/png');
+        imagepng($image);
+        imagedestroy($image);
+        imagedestroy($redimg);
+    }
 
 }
 
