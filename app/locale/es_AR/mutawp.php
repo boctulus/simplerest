8e63e83612741b83105c03c04c1f82240d3df991
Pablo Bozzolo <boctulus@gmail.com>
2023-09-28 12:53:18 +0800

    .

---
+++ app/locale/es_AR/mutawp.php
@@ -0,0 +1,24 @@
+<?php
+
+$intl = [
+	'version'     => 'versión',
+	'support'     => 'soporte',
+	'activate'    => 'activación',
+	'active:/f/'  => 'activa',
+	'active:/m/'  => 'activo',
+	'licence'     => 'licencia',
+	'repository'  => 'repositorio',
+	'plugin'	  => 'plugin',
+	'theme'		  => 'tema',
+	'plugins'	  => 'plugins',
+	'themes'	  => 'temas',
+	'only themes' => 'solo temas',
+	'search'	  => 'buscar',
+	'install'	  => 'instalar',
+	'learn more'  => 'saber más'
+
+	// 'air freight (?-?)' => 'flete aéreo (?-?)',
+	// 'insurance (?%)' => 'seguro (?%)',
+	// 'CIF value' => 'valor CIF',
+	// 'duty (?%)' => 'derechos (?%)',
+];
