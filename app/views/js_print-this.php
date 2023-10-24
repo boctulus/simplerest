dccf6f41c3707dc996ad85f5039128e895e4d4a7
Pablo Bozzolo <boctulus@gmail.com>
2023-10-24 15:25:49 +0800

    .

--- app/views/js_print-this.php
+++ app/views/js_print-this.php
@@ -17,7 +17,11 @@
     }
 </style>
 
-<div>
+<h1>Titulo de la pagina</h1>
+
+<h3>Subtitulo</h3>
+
+<div id="printable">
     <table id="tab"> 
         <tr>
             <th>Bird Name</th>
@@ -38,25 +42,13 @@
 </div>
 
 <p>
-    <input type="button" value="Print Table" onclick="myApp.printTable()" />
+    <input type="button" value="Print Table" onclick="jQuery('#printable').printThis()" />
 </p>
 
+<footer>
+    Creado por Pablo
+</footer>
+
 <script>
-    var myApp = new function () {
-        this.printTable = function () {
-            var tab = document.getElementById('tab');
-
-            var style = "<style>";
-                style = style + "table {width: 100%;font: 17px Calibri;}";
-                style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
-                style = style + "padding: 2px 3px;text-align: center;}";
-                style = style + "</style>";
-
-            var win = window.open('', '', 'height=700,width=700');
-            win.document.write(style);          //  add the style.
-            win.document.write(tab.outerHTML);
-            win.document.close();
-            win.print();
-        }
-    }
+    
 </script>
