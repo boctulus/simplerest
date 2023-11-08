9e189c426ad7e678729886197982c56a7772c067
Pablo Bozzolo <boctulus@gmail.com>
2023-11-08 21:45:27 +0800

    .

--- app/core/templates/Controller.php
+++ app/core/templates/Controller.php
@@ -3,9 +3,7 @@
 namespace __NAMESPACE;
 
 use simplerest\controllers\MyController;
-use simplerest\core\Request;
-use simplerest\core\Response;
-use simplerest\core\libs\Factory;
+use simplerest\core\libs\Strings;
 use simplerest\core\libs\DB;
 
 class __NAME__ extends MyController
