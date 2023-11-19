8f415fd1e7ab42b1ba282097d744e1a09ddcdfe3
Pablo Bozzolo <boctulus@gmail.com>
2023-11-19 15:02:23 +0800

    .

--- app/controllers/apis_tests/WpRestApiController.php
+++ app/controllers/apis_tests/WpRestApiController.php
@@ -3,11 +3,10 @@
 namespace simplerest\controllers\apis_tests;
 
 use simplerest\core\libs\DB;
-use simplerest\core\libs\Strings;
+use simplerest\core\libs\WooCommerceRestAPI;
 use simplerest\core\libs\ApiClient;
 use simplerest\controllers\MyController;
 
-use Automattic\WooCommerce\Client;
 
 /*
     Creacion de credenciales para Basic Auth en la REST API de WooCommerce
@@ -25,79 +24,7 @@ use Automattic\WooCommerce\Client;
     . For non-secure connections (HTTP), you need to use "oAuth1 Authentication".  <--------- !!!
 */
 class WpRestApiController extends MyController
-{    
-    protected $consumer_key    = 'ck_185ddf7e2fa4f631b8a460f3963b1dc818bc5abf';
-    protected $consumer_secret = 'cs_50688595c64b40a0cc1bb610b3852c9ab43be245';
-
-    function via_package(){
-        $base_url = 'http://woo1.lan';
-        $endpoint = '/wp-json/wc/v3/products';
-                       
-        $consumer_key    = 'ck_185ddf7e2fa4f631b8a460f3963b1dc818bc5abf';
-        $consumer_secret = 'cs_50688595c64b40a0cc1bb610b3852c9ab43be245';
-
-        $woocommerce = new Client(
-            $base_url,
-            $consumer_key,
-            $consumer_secret,
-            [
-                'debug'           => true,
-                'return_as_array' => false,
-                'validate_url'    => false,
-                'timeout'         => 30,
-                'ssl_verify'      => false,
-            ]
-        );
-
-        $res = $woocommerce->get($endpoint);
-        dd($res, 'RES');
-    }
-
-
-    function setOAuth( $request ) { 
-        $wc_rest_api = array(
-            'key'    => $this->consumer_key,
-            'secret' => $this->consumer_secret
-        );
-     
-        $consumerKey = $wc_rest_api['key'];
-        $signatureMethod = 'HMAC-SHA1';
-        $timestamp = time();
-        $nonce = md5(mt_rand());
-        $version = '1.0';
-     
-        $oauthSignature = $this->generateSignature( $request, $timestamp, $nonce, $signatureMethod, $version ) ;
-     
-        // echo "Signature: " . $oauthSignature . "<br>"; 
-     
-     
-        return "OAuth oauth_consumer_key=\"{$consumerKey}\",oauth_signature_method=\"{$signatureMethod}\",oauth_timestamp=\"{$timestamp}\",oauth_nonce=\"{$nonce}\",oauth_version=\"1.0\",oauth_signature=\"{$oauthSignature}\"";
-     
-        // return "OAuth oauth_consumer_key={$consumerKey},oauth_signature_method={$signatureMethod},oauth_timestamp={$timestamp},oauth_nonce={$nonce},oauth_version=1.0,oauth_signature={$oauthSignature}";
-     
-    }
-     
-    function generateSignature($request, $timestamp, $nonce, $signatureMethod, $version) {
-        $wc_rest_api = array(
-            'key'    => $this->consumer_key,
-            'secret' => $this->consumer_secret
-        );
-     
-        $base = $request['method'] . "&"
-            . rawurlencode(  $request['url'] ) . "&"
-            . rawurlencode( "oauth_consumer_key=" . $wc_rest_api['key'] )
-            . rawurlencode( "&oauth_nonce=" . $nonce )
-            . rawurlencode( "&oauth_signature_method=" . $signatureMethod )
-            . rawurlencode( "&oauth_timestamp=" . $timestamp )
-            . rawurlencode( "&oauth_version=" . $version );
-            // . rawurlencode( '&' . http_build_query($request['data']) );
-     
-        $key = rawurlencode($wc_rest_api['secret']) . '&';
-        $signature = base64_encode( hash_hmac('sha1', $base, $key, true) );
-     
-        return $signature;
-    }
-
+{   
     // apis_tests wp_rest_api
     function index()
     {
@@ -112,11 +39,13 @@ class WpRestApiController extends MyController
             'url'    => $url
         );
 
+        $woo = new WooCommerceRestAPI('ck_185ddf7e2fa4f631b8a460f3963b1dc818bc5abf', 'cs_50688595c64b40a0cc1bb610b3852c9ab43be245');
+
         $cli = (new ApiClient($url))
         ->withoutStrictSSL()
         ->setHeaders([
             'Content-Type' => 'application/json',
-            'Authorization' => $this->setOAuth( $request),
+            'Authorization' => $woo->setOAuth( $request),
             'User-Agent'   => 'PostmanRuntime/7.34.0',
         ])
 
--- app/core/libs/WooCommerceRestAPI.php
+++ app/core/libs/WooCommerceRestAPI.php
@@ -6,7 +6,58 @@ use simplerest\core\libs\Strings;
 
 class WooCommerceRestAPI
 {
-    function __construct() { }
+    protected $consumer_key;
+    protected $consumer_secret;
+
+    function __construct($consumer_key, $consumer_secret)
+    {
+        $this->consumer_key = $consumer_key;
+        $this->consumer_secret = $consumer_secret;
+    }
+
+    public function setOAuth( $request ) { 
+        $wc_rest_api = array(
+            'key'    => $this->consumer_key,
+            'secret' => $this->consumer_secret
+        );
+     
+        $consumerKey = $wc_rest_api['key'];
+        $signatureMethod = 'HMAC-SHA1';
+        $timestamp = time();
+        $nonce = md5(mt_rand());
+        $version = '1.0';
+     
+        $oauthSignature = $this->generateSignature( $request, $timestamp, $nonce, $signatureMethod, $version ) ;
+     
+        // echo "Signature: " . $oauthSignature . "<br>"; 
+     
+     
+        return "OAuth oauth_consumer_key=\"{$consumerKey}\",oauth_signature_method=\"{$signatureMethod}\",oauth_timestamp=\"{$timestamp}\",oauth_nonce=\"{$nonce}\",oauth_version=\"1.0\",oauth_signature=\"{$oauthSignature}\"";
+     
+        // return "OAuth oauth_consumer_key={$consumerKey},oauth_signature_method={$signatureMethod},oauth_timestamp={$timestamp},oauth_nonce={$nonce},oauth_version=1.0,oauth_signature={$oauthSignature}";
+     
+    }
+     
+    protected function generateSignature($request, $timestamp, $nonce, $signatureMethod, $version) {
+        $wc_rest_api = array(
+            'key'    => $this->consumer_key,
+            'secret' => $this->consumer_secret
+        );
+     
+        $base = $request['method'] . "&"
+            . rawurlencode(  $request['url'] ) . "&"
+            . rawurlencode( "oauth_consumer_key=" . $wc_rest_api['key'] )
+            . rawurlencode( "&oauth_nonce=" . $nonce )
+            . rawurlencode( "&oauth_signature_method=" . $signatureMethod )
+            . rawurlencode( "&oauth_timestamp=" . $timestamp )
+            . rawurlencode( "&oauth_version=" . $version );
+            // . rawurlencode( '&' . http_build_query($request['data']) );
+     
+        $key = rawurlencode($wc_rest_api['secret']) . '&';
+        $signature = base64_encode( hash_hmac('sha1', $base, $key, true) );
+     
+        return $signature;
+    }
 
 
 }
