86c72558504f617cf71128471122f66ef4169f5a
Pablo Bozzolo <boctulus@gmail.com>
2023-12-13 23:18:42 +0800

    .

--- app/shortcodes/star_rating/assets/js/card-slider.js
+++ app/shortcodes/star_rating/assets/js/card-slider.js
@@ -1,9 +1,9 @@
 $('.owl-carousel').owlCarousel({
     loop:false,
-  stagePadding: 15,
+    stagePadding: 15,
     margin:10,
     nav:true,
-  navText : ['<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>','<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'],
+    navText : ['<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>','<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'],
     responsive:{
         0:{
             items:1
@@ -11,11 +11,12 @@ $('.owl-carousel').owlCarousel({
         640:{
             items:2
         },
-      960:{
+        960:{
             items:3
         },
         1200:{
             items:4
         }
-    }
+    },
+    nav : false, 
 })
