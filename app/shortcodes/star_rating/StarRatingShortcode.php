17d2c172b8b48477e0afa8493d79c8fa90e7c8ed
Pablo Bozzolo <boctulus@gmail.com>
2023-12-15 21:04:29 +0800

    .

--- app/shortcodes/star_rating/StarRatingShortcode.php
+++ app/shortcodes/star_rating/StarRatingShortcode.php
@@ -4,21 +4,27 @@ namespace simplerest\shortcodes\star_rating;
 
 class StarRatingShortcode
 {
-    static function get()
-    { 
-        // css_file('third_party/bootstrap/3.x/normalize.css');
+    function __construct(){
+        js_file('third_party/jquery/3.3.1/jquery.min.js');  # external
+    }
 
+    function footer()
+    {
         css_file('https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.css');
         css_file('https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.css');
-        css_file('https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/css/uikit.min.css');
 
+        css_file('https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/css/uikit.min.css');
+        
         #css_file(__DIR__ . '/assets/css/owl-carousel.css');    
         #css_file(__DIR__ . '/assets/css/styles.css');    
 
         // js_file(__DIR__ . '/assets/js/jquery-migrate.min.js', null, true);
 
-        js_file('third_party/jquery/3.3.1/jquery.min.js');              # external
         js_file('third_party/owl_slider/owl.carousel.min.js');          # external
+
+        js_file('third_party/fontawesome/fontawesome_kit.js');          # external
+        #css_file('third_party/fontawesome/all.min.css');                # external
+
         js_file(__DIR__ . '/assets/js/card-slider.js')  ;
 
         js_file('https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js');
@@ -26,4 +32,63 @@ class StarRatingShortcode
         
         return get_view(__DIR__ . '/views/star_rating.php', null);              
     }
+
+    /*
+        La estructura de $data es 
+
+        [
+            "paginator" => [
+                "total"       => {num},  // row_count 
+				"count"       => {num},  // number of rows in the current page
+				"last_page"   => {num},
+				"total_pages" => {num},
+				"page_size"   => {num}
+            ],
+            "rows"      => [
+
+            ]
+
+        ]
+    */
+    function table(Array $data)
+    {
+        css_file('third_party/bootstrap/5.x/bootstrap.min.css');
+        js_file('third_party/bootstrap/5.x/bootstrap.bundle.min.js');
+
+        ?>
+        <div class="container mt-5">
+            <h2>Reviews</h2>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Comentario</th>
+                        <th scope="col">Puntaje</th>
+                        <th scope="col">Cliente</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($data['rows'] as $row) : ?>
+                        <tr>
+                            <td><?php echo $row['comentario']; ?></td>
+                            <td><?php echo $row['puntaje']; ?></td>
+                            <td><?php echo $row['cliente']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+
+            <!-- Paginador -->
+            <nav aria-label="Page navigation">
+                <ul class="pagination">
+                    <?php for ($i = 1; $i <= $data['paginator']['total_pages']; $i++) : ?>
+                        <li class="page-item"><a class="page-link" href="#"><?php echo $i; ?></a></li>
+                    <?php endfor; ?>
+                </ul>
+            </nav>
+        </div>
+
+    <?php
+    }
+
 }
