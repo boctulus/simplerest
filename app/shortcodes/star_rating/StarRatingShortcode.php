d98660e78842ba59d1c7c0a73212e84c01ab2936
Pablo Bozzolo <boctulus@gmail.com>
2023-12-16 20:46:28 +0800

    .

--- app/shortcodes/star_rating/StarRatingShortcode.php
+++ app/shortcodes/star_rating/StarRatingShortcode.php
@@ -62,42 +62,9 @@ class StarRatingShortcode
         css_file('third_party/bootstrap/5.x/bootstrap.min.css');
         js_file('third_party/bootstrap/5.x/bootstrap.bundle.min.js');
 
-        ?>
-        <div class="container mt-5">
-            <!-- <h2>Reviews</h2> -->
-
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Commento</th>
-                        <th scope="col">Punteggio</th>
-                        <th scope="col">Cliente</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($data['rows'] as $row) : ?>
-                        <tr>
-                            <td><?= $row['comment']; ?></td>
-                            <td><?= str_repeat('⭐', $row['score']) ?></td>
-                            <td><?= $row['author']; ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-
-            <!-- Paginador -->
-            <nav aria-label="Page navigation">
-                <ul class="pagination">
-                    <?php for ($i = 1; $i <= $data['paginator']['last_page']; $i++) : 
-                        $page_link = Url::addQueryParam(Url::currentUrl(), 'page', $i);
-                    ?>
-                        <li class="page-item"><a class="page-link" href="<?= $page_link ?>"><?= $i ?></a></li>
-                    <?php endfor; ?>
-                </ul>
-            </nav>
-        </div>
-
-    <?php
+        return get_view(__DIR__ . '/views/star_rating_table.php', [
+            'data' => $data
+        ]); 
     }
 
 }
---
+++ etc/review-generator/py-files/names.py
@@ -0,0 +1,22 @@
+names = [
+    "Alessandro", "Giuseppe", "Francesco", "Mario", "Leonardo", "Luca", "Lorenzo", "Matteo", "Giovanni", "Antonio",
+    "Roberto", "Marco", "Andrea", "Davide", "Angelo", "Stefano", "Michele", "Fabio", "Vincenzo", "Paolo",
+    "Alberto", "Diego", "Simone", "Davide", "Emanuele", "Alessio", "Tommaso", "Christian", "Riccardo", "Filippo",
+    "Federico", "Daniele", "Luca", "Enrico", "Gennaro", "Salvatore", "Emilio", "Marcello", "Claudio", "Sergio",
+    "Piero", "Angelo", "Giorgio", "Massimiliano", "Gino", "Pietro", "Giampaolo", "Umberto", "Domenico", "Raffaele",
+    "Fabrizio", "Alessandro", "Giorgio", "Giovanni", "Luigi", "Mauro", "Alberto", "Luigi", "Stefano", "Paolo",
+    "Francesco", "Andrea", "Roberto", "Marco", "Luca", "Davide", "Simone", "Lorenzo", "Federico", "Matteo",
+    "Alessio", "Giovanni", "Antonio", "Angelo", "Riccardo", "Diego", "Enrico", "Vincenzo", "Christian", "Emanuele",
+    "Fabio", "Filippo", "Michele", "Tommaso", "Giuseppe", "Daniele", "Davide", "Mario", "Alessandro", "Luca",
+    "Leonardo", "Francesco", "Lorenzo", "Giorgio", "Simone", "Marco", "Alberto", "Paolo", "Stefano", "Federico",
+    "Alessio", "Roberto", "Angelo", "Davide", "Giovanni", "Antonio", "Vincenzo", "Matteo", "Christian", "Riccardo",
+    "Fabio", "Michele", "Emanuele", "Tommaso", "Diego", "Daniele", "Enrico", "Simone", "Alessandro", "Mario",
+    "Francesco", "Leonardo", "Luca", "Lorenzo", "Marco", "Giovanni", "Roberto", "Matteo", "Angelo", "Andrea",
+    "Stefano", "Antonio", "Davide", "Alessio", "Michele", "Vincenzo", "Federico", "Paolo", "Christian", "Giuseppe",
+    "Fabio", "Diego", "Emanuele", "Simone", "Riccardo", "Alberto", "Giorgio", "Domenico", "Pietro", "Massimiliano",
+    "Claudio", "Gino", "Mauro", "Sergio", "Angelo", "Piero", "Giampaolo", "Umberto", "Raffaele", "Fabrizio",
+    "Alessandro", "Giorgio", "Giovanni", "Luigi", "Mauro", "Alberto", "Stefano", "Paolo", "Francesco", "Andrea",
+    "Roberto", "Marco", "Luca", "Davide", "Simone", "Lorenzo", "Federico", "Matteo", "Alessio", "Giovanni",
+    "Antonio", "Angelo", "Riccardo", "Diego", "Enrico", "Vincenzo", "Christian", "Emanuele", "Fabio", "Filippo",
+    "Michele", "Tommaso", "Giuseppe", "Daniele", "Davide", "Mario", "Alessandro", "Luca", "Leonardo", "Francesco"
+]
