b2d215c124567711fdf489ec5f29429abd95993c
Pablo Bozzolo <boctulus@gmail.com>
2024-02-02 21:02:49 +0800

    .

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     use simplerest\core\FrontController;
 
     if (config()['router']){        
-        include __DIR__ . '../../config/routes.php';
+        include __DIR__ . '/config/routes.php';
         Route::compile();
         Route::resolve();
     } 
