7394cfd23d225097a63b8be9431de9ec71860476
Pablo Bozzolo <boctulus@gmail.com>
2024-02-23 17:52:15 +0800

    .

---
+++ app/shortcodes/ciudades_cl/CiudadesCLShortcode.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace simplerest\shortcodes\ciudades_cl;
+
+use simplerest\core\libs\DB;
+
+class CiudadesCLShortcode
+{
+    function __construct(){
+        js_file ('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js');    
+        css_file('https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.2.0/dist/select2-bootstrap-5-theme.min.css');
+
+        css_file('https://cdn.jsdelivr.net/npm/select2@4.0.3/dist/css/select2.min.css');
+        js_file('https://cdn.jsdelivr.net/npm/select2@4.0.3/dist/js/select2.min.js');
+
+        $this->index();
+    }
+
+    function index()
+    {   
+        // require_once ABSPATH . PLUGINDIR . '/states-cities-and-places-for-woocommerce/states/CL.php';
+
+        // dd(
+        //     $states ['CL' ]
+        // );
+
+        // require_once ABSPATH . PLUGINDIR . '/states-cities-and-places-for-woocommerce/places/CL.php';
+
+        // dd(
+        //     $places['CL']
+        // );
+
+        $json  = file_get_contents(ETC_PATH . 'countries_states.json');
+
+        set_template('templates/tpl.php');
+
+        view(__DIR__ . '/views/cities_modal.php', [
+            'json' => $json
+        ]);              
+    }
+    
+}
---
+++ app/shortcodes/ciudades_cl/config/config.php
@@ -0,0 +1,34 @@
+<?php
+
+use simplerest\core\libs\Paginator;
+
+/*
+	Falta crea mecanismo para inyectarlo
+*/
+return [
+	
+	'paginator' => [
+		'max_limit' => 50,
+		'default_limit' => 10,
+		'position' => Paginator::TOP,
+		'params'   => [
+			'pageSize' => 'size',
+			'page'	   => 'page_num' // redefinido para WordPress
+		],
+		'formatter' => function ($row_count, $count, $current_page, $page_count, $page_size, $nextUrl){
+			return [
+				"last_page" => $page_count,
+				'paginator' => [
+					"total"       => $row_count, 
+					"count"       => $count,
+					"currentPage" => $current_page,
+					"totalPages"  => $page_count,
+					"pageSize"    => $page_size,
+					"nextUrl"	  => $nextUrl
+				],
+			];
+		},
+	],
+
+	// ...
+];
