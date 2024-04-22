7e035431bd3c347d9338b62b05abccc8f5dfc0c1
Pablo Bozzolo <boctulus@gmail.com>
2024-04-22 11:03:31 +0800

    .

--- app/core/libs/ApiClient.php
+++ app/core/libs/ApiClient.php
@@ -50,6 +50,7 @@ class ApiClient
     const USER_AG_SAFARI  = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15';
     const USER_AGT_EDGE   = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Safari/537.36 Edg/100.0.4896.79';
     const USER_AG_CHROME  = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36';
+    const USER_AG_POSTMAN = 'PostmanRuntime/7.34.0';
 
 
     // Cookies
