2670e45cd166eee3df63203810a902ddd519c33b
Pablo Bozzolo <boctulus@gmail.com>
2024-06-22 19:02:22 +0800

    .

--- app/commands/MysqlLogCommand.php
+++ app/commands/MysqlLogCommand.php
@@ -31,5 +31,17 @@ class MysqlLogCommand implements ICommand
             DB::dbLogDump();
             return;
         }         
-    }    
+    }   
+    
+    function help(){
+        $str = <<<STR
+        php com mysql_log on                                  DB::dbLogOn()
+        php com mysql_log off                                 DB::dbLogOff()
+        php com mysql_log start [-filename=]  que ejecuta ..  DB::dbLogStart()       
+        php com mysql_log dump                                DB::dbLogDump() 
+        STR;
+
+        dd($str, __METHOD__);
+    }
+
 } 
