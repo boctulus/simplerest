0073217bb23eb877f6bf41b892a0e58d87b18305
Pablo Bozzolo <boctulus@gmail.com>
2024-07-22 02:21:19 +0800

    .

---
+++ app/views/test_numeric_input.php
@@ -0,0 +1,13 @@
+<?php
+
+    js_file('js/min-max.js', null, true);
+
+?>
+
+<h1 style="margin-top:10px;">Input test</h1>
+
+<br>
+
+<input id="qty" name="qty" type="number" min="50" step="0.05">
+
+
