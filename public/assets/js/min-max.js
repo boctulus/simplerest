73b1cea842e9a31331f1e14a180eb0e73bc445c9
Pablo Bozzolo <boctulus@gmail.com>
2024-07-22 09:39:12 +0800

    .

--- public/assets/js/min-max.js
+++ public/assets/js/min-max.js
@@ -3,20 +3,14 @@
   y min="" se restringen tanto con las flechas como al ingresar por teclado
 
   @author Pablo Bozzolo < boctulus@gmail.com >
-  @modified by Assistant
-
-  Versión con JS vanilla
-
-  Cambios realizados:
-  - Respeta la cantidad de decimales definida por el atributo step
-  - Usa cero decimales si no hay step definido
 */
 
 document.addEventListener('DOMContentLoaded', function() {
-  const numberInputs = document.querySelectorAll('input[type="number"][max], input[type="number"][min], input[type="number"][step]');
+  const inputQuery = document.querySelectorAll('input[type="number"][max], input[type="number"][min], input[type="number"][step]');
 
-  function roundToStep(value, step) {
-    return Math.round(value / step) * step;
+  function roundToStep(value, step, min = 0, round_fn = Math.ceil) {
+    const invStep = 1 / step;
+    return round_fn((value - min) * invStep) / invStep + min;
   }
 
   function countDecimals(value) {
@@ -24,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
     return value.toString().split(".")[1].length || 0;
   }
 
-  numberInputs.forEach(function(input) {
+  inputQuery.forEach(function(input) {
     let timer;
 
     input.addEventListener('keyup', function() {
