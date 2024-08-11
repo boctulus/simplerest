b0f49589faadcdc7df5f38199d11d77b3eb4dd3c
Pablo Bozzolo <boctulus@gmail.com>
2024-08-11 11:00:42 +0800

    .

--- app/controllers/FbController.php
+++ app/controllers/FbController.php
@@ -11,12 +11,31 @@ class FbController extends Controller
     function index(){
         $grupos = <<<STR
         WP | ES
+        https://web.facebook.com/groups/313520438797399/
+        https://web.facebook.com/groups/1758845590845832/
         https://www.facebook.com/groups/wpcrc
         https://www.facebook.com/groups/wptodos
         https://www.facebook.com/groups/AprendeWordpress
         https://www.facebook.com/groups/1523799777739104/
         https://www.facebook.com/groups/1031989716925981/
         https://www.facebook.com/groups/1290208751055228/
+        https://web.facebook.com/groups/7094894653/
+        https://web.facebook.com/groups/496610637551676/
+        https://web.facebook.com/groups/ayudawp
+        https://web.facebook.com/groups/140662189459719/
+        https://web.facebook.com/groups/2813292022223049
+        https://web.facebook.com/groups/wpargentina/
+        https://web.facebook.com/groups/111122896249184/
+        https://web.facebook.com/groups/1149775045474205/
+        https://web.facebook.com/groups/1485989378282535/
+
+        WOO | ES
+        https://web.facebook.com/groups/450025545191592/
+        https://web.facebook.com/groups/grupodewoocommerce
+        https://web.facebook.com/groups/170857576608884/
+
+        LearnDash | ES
+        https://web.facebook.com/groups/780588969003381/
 
         PHP | ES
         https://www.facebook.com/groups/455489051242688/
@@ -34,6 +53,8 @@ class FbController extends Controller
         https://www.facebook.com/groups/378513999208093
         https://www.facebook.com/groups/1216288588385133/
         https://www.facebook.com/groups/1656457771305611/
+        https://web.facebook.com/groups/311323871209420/
+        https://web.facebook.com/groups/1051533001572433/
 
         Testing y QA | ES
         https://www.facebook.com/groups/1616115831782049/
@@ -50,10 +71,6 @@ class FbController extends Controller
         WP - WOO | IT
         https://www.facebook.com/groups/1380949515301271/
         https://www.facebook.com/groups/wpitalyplus/
-        https://www.facebook.com/groups/104873832995379/
-
-        ECommerce | IT
-        https://www.facebook.com/groups/104873832995379/
 
         PHP | IT
         https://www.facebook.com/groups/377041959088599
@@ -61,7 +78,7 @@ class FbController extends Controller
         https://www.facebook.com/groups/336426883084246/
 
         Programación | IT
-        https://www.facebook.com/groups/590801050986657/
+        https://www.facebook.com/groups/353299598353917/
 
         Programación | RO (Rumanía)
         https://www.facebook.com/groups/677877235598824/
@@ -84,6 +101,8 @@ class FbController extends Controller
                 continue;
             }
             
+            $line = str_replace('https://www.', 'https://web.', $line);
+            
             if (strpos($line, 'http') === false) {
                 // Es un título de sección
                 if (!empty($current_section)) {
@@ -95,6 +114,11 @@ class FbController extends Controller
                 // Es un enlace
                 $current_links[] = $line;
             }
+        }
+
+        // Eliminar enlaces duplicados en cada seccion
+        foreach ($sections as $ix => $section){
+            $sections[$ix] = array_unique($section);
         }
 
         // Añadir la última sección
