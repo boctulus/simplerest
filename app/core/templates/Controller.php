1f795d15ba21634cb2e9bf86b8c8fb59a20ff6b8
Pablo Bozzolo <boctulus@gmail.com>
2024-09-15 12:37:43 +0800

    .

--- app/core/templates/Controller.php
+++ app/core/templates/Controller.php
@@ -5,13 +5,11 @@ namespace __NAMESPACE;
 use simplerest\core\controllers\Controller;
 use simplerest\core\libs\Strings;
 use simplerest\core\libs\DB;
+use simplerest\core\traits\TimeExecutionTrait;
 
 class __NAME__ extends Controller
 {
-    function __construct()
-    {
-        parent::__construct();        
-    }
+    function __construct() { parent::__construct(); }
 
     function index()
     {
