d65d87d4410c1add3991c64d4e1aa4042597cc10
Pablo Bozzolo <boctulus@gmail.com>
2024-09-24 16:53:39 +0800

    .

---
+++ app/controllers/PromptGeneratorController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace simplerest\controllers;
+
+use simplerest\core\controllers\Controller;
+use simplerest\core\libs\Strings;
+use simplerest\core\libs\DB;
+use simplerest\core\traits\TimeExecutionTrait;
+
+/*
+    https://chatgpt.com/c/66f277ae-259c-8004-95a5-a3a9f37cb7fd
+
+    TO-DO
+
+    - Generar la API a fin de poder alimentar el form
+*/
+class PromptGeneratorController extends Controller
+{
+    function __construct() { parent::__construct(); }
+
+    function index()
+    {
+        view('prompt_generator/index.php', [
+            'title' => 'Generador de Prompt'
+        ], 'templates/tpl_bt5.php');                  
+    }
+}
+
