65104f54a063bd3a2267ab14ede5ab0e5550ef3c
Pablo Bozzolo <boctulus@gmail.com>
2024-09-24 21:42:20 +0800

    .

--- app/views/bir/invoice_layout.php
+++ app/views/bir/invoice_layout.php
@@ -1,110 +1,106 @@
 <div class="invoice-box">
-    <div class="text-center" style="line-height: 1;">
-        <h2 class="fw-bold">ADOON COMPUTER PROGRAMMING SERVICES</h2>
-        <p style="margin-top: 20px;">PUROK 7, POB. 1, SANTA TERESITA, BATANGAS</p>
-        <p><strong>REVELYN R. PADUA</strong> - Prop.</p>
-        <p>NON-VAT Reg. TIN 322-610-092-000</p>
+<div class="text-center" style="line-height: 1;">
+    <h2 class="fw-bolder">ADOON COMPUTER PROGRAMMING SERVICES</h2>
+        <p style="margin-top: 20px; font-weight: 600;">PUROK 7, POB. 1, SANTA TERESITA, BATANGAS</p>
+        <p style="font-weight: 600;"><strong>REVELYN R. PADUA</strong> - Prop.</p>
+        <p style="font-weight: 600;">NON-VAT Reg. TIN 322-610-092-000</p>
         
-        <h3 class="fw-bold" style="margin-top: 20px;">SALES INVOICE</h3>
-    </div>
-
-    <!-- Cabecera alineada -->
-    <!-- TODO: achicar interlineado -->
-    <div class="d-flex justify-content-between mt-4" >
-        <div>
-            <p><strong>Sold to:</strong> ___________________________________________________</p>
-            <p><strong>Business Style:</strong> ____________________________________________</p>
-            <p><strong>TIN/SC-TIN:</strong> _______________________________________________</p>
-            <p><strong>Address:</strong> ___________________________________________________</p>
-        </div>
-        <div class="text-end">
-            <p><strong>Date:</strong> _____________________________</p>
-            <p><strong>Terms:</strong> ____________________________</p>
-            <p><strong>OSCA/PWD ID No:</strong> ____________________</p>
-        </div>
-    </div>
-
-    <!-- Tabla de artículos -->
-    <table class="table table-bordered mt-4" id="articles">
-        <thead class="table-light text-center">
-            <tr>
-                <th width="10%">QTY</th>
-                <th width="15%">UNIT</th>
-                <th>ARTICLES</th>
-                <th width="15%">UNIT PRICE</th>
-                <th width="15%">AMOUNT</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- TODO: deben tener bordes -->
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
-
-    <!-- Totales con líneas -->
-    <table class="table table-bordered" style="margin-top:-17px;">
-        <tbody>
-            <tr>
-                <td><strong>TOTAL AMOUNT DUE:</strong></td>
-                <td width="15%"></td>
-            </tr>
-        </tbody>
-    </table>
-
-   
+        <h3 class="fw-bolder" style="margin-top: 20px;">SALES INVOICE</h3>
 </div>
+
+        <!-- Cabecera alineada -->
+        <div class="d-flex justify-content-between mt-4">
+            <div>
+                <p><strong>Sold to:</strong> ___________________________________________________</p>
+                <p><strong>Business Style:</strong> ____________________________________________</p>
+                <p><strong>TIN/SC-TIN:</strong> _______________________________________________</p>
+                <p><strong>Address:</strong> ___________________________________________________</p>
+            </div>
+            <div class="text-end">
+                <p><strong>Date:</strong> _____________________________</p>
+                <p><strong>Terms:</strong> ____________________________</p>
+                <p><strong>OSCA/PWD ID No:</strong> ____________________</p>
+            </div>
+        </div>
+
+        <!-- Tabla de artículos -->
+        <table class="table table-bordered mt-4" id="articles">
+            <thead class="table-light text-center">
+                <tr>
+                    <th width="10%">QTY</th>
+                    <th width="15%">UNIT</th>
+                    <th>ARTICLES</th>
+                    <th width="15%">UNIT PRICE</th>
+                    <th width="15%">AMOUNT</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <!-- Totales con líneas -->
+        <table class="table table-bordered" style="margin-top:-17px;">
+            <tbody>
+                <tr>
+                    <td><strong>TOTAL AMOUNT DUE:</strong></td>
+                    <td width="15%"></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
