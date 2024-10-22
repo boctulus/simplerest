7f93ff64b1dbcf3cd9e0817594621b24cfa1e97a
Pablo Bozzolo <boctulus@gmail.com>
2024-10-22 19:33:33 +0800

    .

--- app/libs/ProductPDF.php
+++ app/libs/ProductPDF.php
@@ -6,7 +6,18 @@ use simplerest\core\libs\Strings;
 
 use Fpdf\Fpdf;
 
+/*
+    Ej de uso:
+
+    $pdf = new ProductPDF();
+    $pdf->setData($product);
+    $pdf->render();            
+
+    $pdf->Output();
+*/
 class ProductPDF extends Fpdf {
+    protected $data;
+
     public function __construct(
         $orientation = 'P',
         $unit = 'mm',
@@ -16,18 +27,9 @@ class ProductPDF extends Fpdf {
         // ...
     }
 
-    function getImageString($url) {
-        $image_data = file_get_contents($url);
-        $image_info = getimagesize($url);
-
-        // Obtener el tipo de imagen desde la URL
-        $extension = strtolower(pathinfo($url, PATHINFO_EXTENSION));
-        dd($extension, 'EXT');
-
-        return [
-            'data' => $image_data,
-            'type' => $extension
-        ];
+    function setData(array $product){
+        $this->data = $product;
+        return $this;
     }
 
     // Cabecera de página
@@ -48,6 +50,41 @@ class ProductPDF extends Fpdf {
             "Nuestras oficinas se encuentran en Santa Petronila 955, Quinta Normal, Santiago\n" .
             "contacto@tienda.relmotor.cl | www.relmotor.cl",
             0, 'C');
+    }
+
+    function render(){
+        $this->AddPage();
+
+        // Datos del producto
+        
+        // Agregar imagen
+        $extension = strtoupper(pathinfo($this->data['featured_image'], PATHINFO_EXTENSION));
+        $this->Image($this->data['featured_image'], 10, 40, 90, $extension);
+
+        // Información del producto
+        $this->SetFont('Arial', 'B', 12);
+        $this->SetTextColor(0, 32, 96); // Azul oscuro para SKU
+        $this->SetXY(10, 140);
+        $this->Cell(0, 10, 'SKU: ' . $this->data['sku']);
+
+        // Descripción
+        $this->SetFont('Arial', '', 11);
+        $this->SetTextColor(0);
+        $this->SetXY(10, 150);
+        $this->MultiCell(0, 5, $this->data['description']);
+
+        // Marca
+        $this->SetXY(10, 170);
+        $this->SetFont('Arial', 'B', 11);
+        $this->Cell(30, 10, 'Marca');
+        $this->SetFont('Arial', '', 11);
+        $this->Cell(0, 10, $this->data['attributes'][0]['value']);
+
+        // Precio
+        $this->SetXY(10, 180);
+        $this->SetFont('Arial', 'B', 14);
+        $this->SetTextColor(255, 0, 0); // Rojo para el precio
+        $this->Cell(0, 10, 'Precio: $' . number_format($this->data['price'], 0, ',', '.') . ' Neto');
     }
 }
 
