9d94874211ec711f8a463f5f5525af1805bd5492
Pablo Bozzolo <boctulus@gmail.com>
2024-11-02 15:39:13 +0800

    .

--- app/schemas/az/Relations.php
+++ app/schemas/az/Relations.php
@@ -1,7 +1,115 @@
 <?php 
 
 return [
-        'relation_type'=> array (
+        'related_tables' => array (
+  'api_keys' => 
+  array (
+    0 => 'users',
+  ),
+  'automoviles' => 
+  array (
+    0 => 'medios_transporte',
+  ),
+  'boletas' => 
+  array (
+    0 => 'users',
+  ),
+  'book_reviews' => 
+  array (
+    0 => 'books',
+  ),
+  'books' => 
+  array (
+    0 => 'users',
+    1 => 'book_reviews',
+  ),
+  'collections' => 
+  array (
+    0 => 'users',
+  ),
+  'facturas' => 
+  array (
+    0 => 'users',
+  ),
+  'facturas4' => 
+  array (
+    0 => 'users',
+  ),
+  'files' => 
+  array (
+    0 => 'users',
+  ),
+  'folder_other_permissions' => 
+  array (
+    0 => 'users',
+  ),
+  'folder_permissions' => 
+  array (
+    0 => 'users',
+  ),
+  'folders' => 
+  array (
+    0 => 'users',
+  ),
+  'medios_transporte' => 
+  array (
+    0 => 'automoviles',
+  ),
+  'product_categories' => 
+  array (
+    0 => 'products_product_categories',
+  ),
+  'product_comments' => 
+  array (
+    0 => 'products',
+  ),
+  'products' => 
+  array (
+    0 => 'users',
+    1 => 'product_comments',
+    2 => 'products_product_categories',
+  ),
+  'products_product_categories' => 
+  array (
+    0 => 'products',
+    1 => 'product_categories',
+  ),
+  'sp_permissions' => 
+  array (
+    0 => 'user_sp_permissions',
+  ),
+  'user_roles' => 
+  array (
+    0 => 'users',
+  ),
+  'user_sp_permissions' => 
+  array (
+    0 => 'sp_permissions',
+    1 => 'users',
+  ),
+  'user_tb_permissions' => 
+  array (
+    0 => 'users',
+  ),
+  'users' => 
+  array (
+    0 => 'api_keys',
+    1 => 'boletas',
+    2 => 'books',
+    3 => 'collections',
+    4 => 'facturas',
+    5 => 'facturas4',
+    6 => 'files',
+    7 => 'folder_other_permissions',
+    8 => 'folder_permissions',
+    9 => 'folders',
+    10 => 'products',
+    11 => 'user_roles',
+    12 => 'user_sp_permissions',
+    13 => 'user_tb_permissions',
+  ),
+),
+        'relation_type'  => array (
   'api_keys~users' => 'n:1',
   'automoviles~medios_transporte' => '1:n',
   'boletas~users' => 'n:1',
@@ -21,8 +129,8 @@ return [
   'products~users' => 'n:1',
   'products~product_comments' => '1:1',
   'products~products_product_categories' => '1:n',
-  'products_product_categories~product_categories' => 'n:1',
   'products_product_categories~products' => 'n:1',
+  'products_product_categories~product_categories' => 'n:1',
   'sp_permissions~user_sp_permissions' => '1:n',
   'user_roles~users' => 'n:1',
   'user_sp_permissions~sp_permissions' => 'n:1',
@@ -47,7 +155,7 @@ return [
   'sp_permissions~users' => 'n:m',
   'users~sp_permissions' => 'n:m',
 ),
-        'multiplicity' => array (
+        'multiplicity'   => array (
   'api_keys~users' => false,
   'automoviles~medios_transporte' => true,
   'boletas~users' => false,
@@ -67,8 +175,8 @@ return [
   'products~users' => false,
   'products~product_comments' => false,
   'products~products_product_categories' => true,
-  'products_product_categories~product_categories' => false,
   'products_product_categories~products' => false,
+  'products_product_categories~product_categories' => false,
   'sp_permissions~user_sp_permissions' => true,
   'user_roles~users' => false,
   'user_sp_permissions~sp_permissions' => false,
@@ -92,5 +200,5 @@ return [
   'products~product_categories' => true,
   'sp_permissions~users' => true,
   'users~sp_permissions' => true,
-)
-        ];
+),
+];
