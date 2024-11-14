3981139f7aa4500fc25033c4106fa07dc2d376a8
Pablo Bozzolo <boctulus@gmail.com>
2024-11-15 07:19:51 +0800

    Automatic update

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <?php
 
-	require_once __DIR__ . '/app.php';
-    
-    use simplerest\core\Route;
-    use simplerest\core\FrontController;
+/*
+    Framework Name: Simplerest
+    Description: API Rest framework for PHP
+    Author: Pablo Bozzolo
+    Author Email: boctulus@gmail.com
+*/
 
-    if (config()['router']){        
-        include __DIR__ . '/config/routes.php';
-        Route::compile();
-        Route::resolve();
-    } 
+require_once __DIR__ . '/app.php';
 
-    if (config()['front_controller']){        
-        FrontController::resolve();
-    } 
+use simplerest\core\Route;
+use simplerest\core\FrontController;
 
-    //throw new Exception('There is no Router or FrontController enabled!');
+if (config()['router']){        
+    include __DIR__ . '/config/routes.php';
+    Route::compile();
+    Route::resolve();
+} 
 
-	
+if (config()['front_controller']){        
+    FrontController::resolve();
+} 
+
+//throw new Exception('There is no Router or FrontController enabled!');
+
+
 
 
 
