7e01b14f92b145ec3deb865f54a9292932dd9aed
Pablo Bozzolo <boctulus@gmail.com>
2024-12-13 11:20:55 +0100

    PowerConsumption mejorada

--- app/libs/PowerConsumption.php
+++ app/libs/PowerConsumption.php
@@ -8,6 +8,9 @@ use simplerest\core\libs\Strings;
 
 class PowerConsumption {
     const MAX_CONSUMPTION =  300;
+    const ANSI_RED = "\033[31m";
+    const ANSI_YELLOW = "\033[33m";
+    const ANSI_RESET = "\033[0m";
 
 
     static function first(){
@@ -54,12 +57,39 @@ class PowerConsumption {
         $averageConsumption = $dailyConsumption / $daysElapsed;
         $excess = $dailyConsumption - self::MAX_CONSUMPTION;
 
+        // Calculamos el consumo diario máximo permitido
+        $maxDailyConsumption = self::MAX_CONSUMPTION / 30;
+        
+        // Calculamos el porcentaje actual respecto al máximo
+        $currentPercentage = ($averageConsumption * 30 / self::MAX_CONSUMPTION) * 100;
+        
+        // Días restantes del mes
+        $daysInMonth = date('t');
+        $remainingDays = $daysInMonth - date('d');
+
+        $message = 'OK. Dentro de los limites';
+        if ($excess >= 0) {
+            $message = self::ANSI_RED . 'Excedido!!!' . self::ANSI_RESET;
+        } elseif ($remainingDays <= 7 && $currentPercentage >= 95) {
+            $message = self::ANSI_YELLOW . 'Advertencia: Cerca del límite!' . self::ANSI_RESET;
+        }
+
         return [
             'date' => $today,
             'consumption' => $dailyConsumption,
             'average_consumption' => round($averageConsumption, 2),
             'excess' => $excess,
-            'message' => ($excess < 0) ? 'OK. Dentro de los limites' : 'Excedido!!!'
+            'message' => $message,
+            'readings' => [
+                'first' => [
+                    'value' => $first['reading'],
+                    'date' => $first['created_at']
+                ],
+                'last' => [
+                    'value' => $finalReading,
+                    'date' => $today
+                ]
+            ]
         ];
     }
 
