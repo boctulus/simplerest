37e052f8f3ad3d81fb9cc6ef952d736fedcb3c8a
Pablo Bozzolo <boctulus@gmail.com>
2025-01-05 02:35:33 +0100

    Automatic update

--- app/migrations/products/2025_01_05_132289631_products.php
+++ app/migrations/products/2025_01_05_132289631_products.php
@@ -25,11 +25,35 @@ class Products implements IMigration
 
         $sc = new Schema($this->table);
 
-        $sc
-        ->integer('id')->auto()->pri()
-        ->varchar('name')
-        ->float('price')
-		// ...
+        $sc->integer('id')->auto()->pri()
+        ->varchar('name', 80)
+        ->varchar('type', 20)->nullable()
+        ->varchar('regular_price')->nullable()
+        ->varchar('sale_price')->nullable()
+        ->text('description')
+        ->varchar('short_description', 512)->nullable()
+        ->varchar('slug', 100)->unique()
+        ->text('images')
+        ->varchar('categories', 250)->nullable()
+        ->varchar('tags', 250)->nullable()
+        ->text('dimensions')->nullable()
+        ->text('attributes')->nullable()
+        ->varchar('sku', 50)->nullable()
+        ->varchar('status', 20)->nullable()
+        ->integer('stock')->nullable()
+        ->varchar('stock_status', 30)->nullable()
+        ->varchar('url_ori', 300)->nullable()->unique()
+        ->integer('posted')->nullable()
+        ->varchar('comment', 200)->nullable()
+        ->datetime('created_at')
+        ->datetime('updated_at')->nullable()
+        ->decimal('cost', 10, 2)->nullable()
+        ->varchar('size', 20)->nullable()
+        ->integer('belongs_to')->nullable()
+        ->integer('active')->nullable()
+        ->integer('locked')->nullable()
+        ->varchar('workspace', 50)->nullable()
+        ->timestamp('deleted_at')->nullable()
         ->datetimes();
 
         $sc->create();
