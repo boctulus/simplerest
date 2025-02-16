f850587109dc4a96f266183812d891cffaff9dc7
Pablo Bozzolo <boctulus@gmail.com>
2025-02-17 07:47:07 +0800

    Prompt generator ahora procesa directorios

--- app/controllers/api/Prompts.php
+++ app/controllers/api/Prompts.php
@@ -7,6 +7,8 @@ use simplerest\core\libs\Files;
 use simplerest\core\libs\Strings;
 use simplerest\core\libs\ApiClient;
 use simplerest\controllers\MyApiController; 
+use simplerest\core\exceptions\NotFileButDirectoryException;
+use simplerest\core\libs\Logger;
 
 class Prompts extends MyApiController
 { 
@@ -28,12 +30,45 @@ class Prompts extends MyApiController
     
     protected function onPostingAfterCheck($id, array &$data)
     { 
+        // maximo numero de archivos a cargar de un directorio
+        $max_files = 100; 
+
         $data['content'] = [];
 
         $base_path = $data['base_path'] ?? null;
 
+        $data['files'] = array_unique($data['files']);
+
         foreach ($data['files'] as $file_path) {
-            $data['content'][] = Files::getContent($file_path, $base_path);
+            try {
+                $data['content'][$file_path] = Files::getContent($file_path, $base_path);
+            } catch (NotFileButDirectoryException $e){
+                if ($base_path !== null && !Files::isAbsolutePath($file_path)) {
+                    $path = Files::removeFirstSlash($file_path);
+                    $path = Files::addTrailingSlash($base_path) . DIRECTORY_SEPARATOR . $file_path;
+                } else {
+                    $path = $file_path;
+                }
+
+                // Logger::dd($path, 'PATH');
+
+                $files = Files::recursiveGlob($path . Files::convertSlashes('/') . '*.*');
+
+                if (count($files) > $max_files){
+                    throw new \Exception("So many files to read");
+                }
+                
+                // Logger::dd($files, 'FILEs');
+
+                /*
+                    Cuando es un directorio toca todavia en la el JS de la vista manejarlo:
+                */
+                foreach ($files as $file){  
+                    $data['content'][$file] = file_get_contents($file);
+                    // Logger::dd($file, 'FILE');
+                }
+            } 
+            
         }
     }
 } 
