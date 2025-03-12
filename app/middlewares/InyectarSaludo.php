b29ba4d8f052180e52a704dc2fe336b9b95366e9
Pablo Bozzolo <boctulus@gmail.com>
2025-03-12 10:18:20 +0800

    Middleware funcionando en WebRouter

--- app/middlewares/InyectarSaludo.php
+++ app/middlewares/InyectarSaludo.php
@@ -3,28 +3,17 @@
 namespace simplerest\middlewares;
 
 use simplerest\core\Middleware;
-use simplerest\core\libs\Factory;
-use simplerest\core\libs\Schema;
-use simplerest\core\Model;
-use simplerest\core\libs\DB;
 use simplerest\core\libs\Strings;
 
 class InyectarSaludo extends Middleware
 {   
-    function __construct()
-    {
-        parent::__construct();
-    }
-
-    function handle(?callable $next = null){
-        $data = $this->res;
+    function handle(){
+        $data = response()->get();
 
         if (is_string($data)){
             if (Strings::startsWith('Hello ', $data)){
-                $this->res->set(preg_replace('/Hello (.*)/', "Hello happy $1", $data,1));
+                response()->set(preg_replace('/Hello (.*)/', "Hello happy $1", $data,1));
             }
         }
-
-        //$next($this->req, $this->res);
     }
 }
