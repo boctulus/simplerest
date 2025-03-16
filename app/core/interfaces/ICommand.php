fe2860e28776b530b7735b3c34e8d9fec687449b
Pablo Bozzolo <boctulus@gmail.com>
2025-03-16 23:31:20 +0800

    Automatic update

--- app/core/interfaces/ICommand.php
+++ app/core/interfaces/ICommand.php
@@ -2,6 +2,12 @@
 
 namespace simplerest\core\interfaces;
 
+/*
+    Posibilidad de mejoras
+
+    https://chatgpt.com/c/67d6ed9d-7454-800d-a9be-395095c69518
+*/
+
 interface ICommand {
     function handle($args);
     function help($name = null, ...$args);
