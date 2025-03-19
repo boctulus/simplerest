ace87ece28d8ac712b65180fff5ba966f7d5b845
Pablo Bozzolo <boctulus@gmail.com>
2025-03-19 18:32:53 +0800

    Automatic update

--- app/core/templates/ConsoleController.php
+++ app/core/templates/ConsoleController.php
@@ -2,11 +2,11 @@
 
 namespace __NAMESPACE__;
 
-use simplerest\core\controllers\ConsoleController;
+use simplerest\core\libs\DB;
 use simplerest\core\Request;
 use simplerest\core\Response;
 use simplerest\core\libs\Factory;
-use simplerest\core\libs\DB;
+use simplerest\core\controllers\ConsoleController;
 
 class __NAME__ extends ConsoleController
 {
