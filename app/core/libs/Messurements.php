6d2e8327bfe35c8481e74a5ecd7cfca17bacb2ed
Pablo Bozzolo <boctulus@gmail.com>
2025-03-20 21:00:14 +0800

    Automatic update

--- app/core/libs/Messurements.php
+++ app/core/libs/Messurements.php
@@ -10,18 +10,18 @@ namespace simplerest\core\libs;
 
 class Messurements
 {
-    static function toInches(float $feets, $inches = 0) {
+    static function feetToInches(float $feets, $inches = 0) {
         $feet_into_inches = (float) $feets * 12;
         $inches           = (float) $inches;
        
         return $feet_into_inches + $inches;
     }
 
-    static function toFeet(float $inches){
+    static function inchesToFeet(float $inches){
         return $inches / 12;
     }
 
-    static function toFeetAndInches(float $inches) {
+    static function inchesToFeetAndInches(float $inches) {
         $div = $inches / 12;
         $feets  = floor($div);
         $inches = floor(($div - $feets) * 12);
