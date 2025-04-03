9c50076e2e39dccdd746289a5670e8bb179d2041
Pablo Bozzolo <boctulus@gmail.com>
2025-04-03 15:43:54 +0800

    make::package() mejorado

---
+++ packages/boctulus/other/src/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Boctulus\Simplerest\packages;
+
+class Other 
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       
+    }     
+}
+
