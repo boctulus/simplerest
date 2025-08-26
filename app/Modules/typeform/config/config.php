156778ad939ad7a080fdcd558361a850d2797897
Pablo Bozzolo <boctulus@gmail.com>
2025-08-26 18:59:19 +0700

    TOS

--- app/Modules/typeform/config/config.php
+++ app/Modules/typeform/config/config.php
@@ -1,10 +1,16 @@
 <?php 
 
 /*
-    Module config's file
+    Typeform Module Configuration File
 
-    Settings accesible as '{module-name}.key' using Config::get()
+    Settings accessible as 'typeform.key' using Config::get()
 */
 return [
-    // ...
+	"links" => [
+		// Terms of Service link - can be absolute or relative
+		// Examples:
+		// Absolute: "https://example.com/terms-and-conditions"
+		// Relative: "/terms-and-conditions" or "terms-and-conditions"
+		"tos" => "https://friendlypos.cl/terminos-y-condiciones/"
+	]
 ];
