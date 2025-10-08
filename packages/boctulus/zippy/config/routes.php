9a7f5ed38764cd62b2bcd0854190bab77edfa0dc
Pablo Bozzolo <boctulus@gmail.com>
2025-10-08 12:16:57 +0700

    Corregidos problemas con CliRouter

--- packages/boctulus/zippy/config/routes.php
+++ packages/boctulus/zippy/config/routes.php
@@ -12,10 +12,10 @@ WebRouter::get('zippy/csv/products', 'Boctulus\Zippy\Controllers\ZippyController
 WebRouter::get('zippy/csv/sucursales', 'Boctulus\Zippy\Controllers\ZippyController@read_csv_sucursales');
 
 // ProductImportController routes (ZippyProductsController.php)
-WebRouter::get('zippy/vea', 'Boctulus\Zippy\Controllers\ProductImportController@index');  // deberia ir al final, es la ruta mas general del "grupo"
-WebRouter::get('zippy/vea/import', 'Boctulus\Zippy\Controllers\ProductImportController@import_zippy_csv');
-WebRouter::get('zippy/vea/check-dupes', 'Boctulus\Zippy\Controllers\ProductImportController@check_dupes');
+WebRouter::get('zippy/importer', 'Boctulus\Zippy\Controllers\ProductImportController@index'); 
+WebRouter::get('zippy/importer/import', 'Boctulus\Zippy\Controllers\ProductImportController@import_zippy_csv');
+WebRouter::get('zippy/importer/check-dupes', 'Boctulus\Zippy\Controllers\ProductImportController@check_dupes');
 
 // ZippyUsersController routes
-WebRouter::get('zippy/users', 'Boctulus\Zippy\Controllers\ZippyUsersController@index'); // deberia ir al final, es la ruta mas general del "grupo"
+WebRouter::get('zippy/users', 'Boctulus\Zippy\Controllers\ZippyUsersController@index'); 
 WebRouter::get('zippy/users/login', 'Boctulus\Zippy\Controllers\ZippyUsersController@login');
