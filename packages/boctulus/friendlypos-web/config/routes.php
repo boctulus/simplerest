d9c298b2dc74a7bbd5bc9371fe6917909d5782d9
Pablo Bozzolo <boctulus@gmail.com>
2025-11-03 09:39:24 +0700

    Automatic update

--- packages/boctulus/friendlypos-web/config/routes.php
+++ packages/boctulus/friendlypos-web/config/routes.php
@@ -8,15 +8,15 @@ use Boctulus\Simplerest\Core\WebRouter;
     Rutas para generación de PDFs de comprobantes FriendlyPOS
 */
 
-// Generar PDF de venta por ID
-WebRouter::get('pdf/venta/{id}', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@generarPdf');
+// // Generar PDF de venta por ID
+// WebRouter::get('pdf/venta/{id}', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@generarPdf');
 
-// Generar PDF forzando tipo de documento
-WebRouter::get('pdf/venta/{id}/tipo/{tipo}', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@generarPdfEspecial');
+// // Generar PDF forzando tipo de documento
+// WebRouter::get('pdf/venta/{id}/tipo/{tipo}', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@generarPdfEspecial');
 
-// Generar ticket de venta
-WebRouter::post('pdf/ticket', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@generarTicketPdf');
+// // Generar ticket de venta
+// WebRouter::post('pdf/ticket', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@generarTicketPdf');
 
-// Rutas de testing
-WebRouter::get('pdf/test', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@testBoletaPdf');
-WebRouter::get('pdf/ventas/listar', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@listarVentas');
+// // Rutas de testing
+// WebRouter::get('pdf/test', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@testBoletaPdf');
+// WebRouter::get('pdf/ventas/listar', 'Boctulus\FriendlyposWeb\Controllers\ComprobantePdfController@listarVentas');
