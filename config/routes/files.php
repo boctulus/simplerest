82e6684f61e35f747255112fb473485f944aa182
Pablo Bozzolo <boctulus@gmail.com>
2026-06-17 10:41:21 +0800

    Automatic update

--- config/routes/files.php
+++ config/routes/files.php
@@ -2,6 +2,19 @@
 
 use Boctulus\Simplerest\Core\WebRouter;
 
+// GET api/v1/files/{id} -> obtiene info del archivo (id = uuid). Ruta dedicada con placeholder
+WebRouter::get('api/v1/files/{id}', function($id){
+    $controller = new \Boctulus\Simplerest\Core\Api\Files();
+    $ret = $controller->get($id);    
+	\Boctulus\Simplerest\Core\Libs\Factory::response()->flush();
+});
+
+// GET api/v1/files -> listado de archivos (usa index() del ApiController)
+WebRouter::get('api/v1/files', function(){
+    $controller = new \Boctulus\Simplerest\Core\Api\Files();
+    $ret = $controller->get();    
+	\Boctulus\Simplerest\Core\Libs\Factory::response()->flush();
+});
 
 // POST api/v1/files -> upload (multipart/form-data, campo `file`, soporta varios)
 WebRouter::post('api/v1/files', function(){
@@ -22,10 +35,10 @@ WebRouter::delete('api/v1/files/{id}', function($id){
 	\Boctulus\Simplerest\Core\Libs\Factory::response()->flush();
 });
 
-// GET get/{id} -> descarga/sirve el archivo por id (= uuid). Download::get() hace
+// GET download/{id} -> descarga/sirve el archivo por id (= uuid). Download::get() hace
 // readfile()+exit (no usa response()->send), así que no requiere flush(). Es el destino
-// del `link` que devuelve el upload (base_url()/get/<uuid>).
-WebRouter::get('get/{id}', function($id){
+// del `link` que devuelve el upload (base_url()/download/<uuid>).
+WebRouter::get('download/{id}', function($id){
 	$controller = new \Boctulus\Simplerest\Core\Api\Download();
 	$controller->get($id);
 });
